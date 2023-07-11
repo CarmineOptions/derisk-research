740cc9e3f9b95ebbc8a87543039c99a98206899c
ella <ellaculleton13@berkeley.edu>
2023-07-11 11:28:42 +0000

    fix decimals and add one vis

--- liquidation_playground.py
+++ liquidation_playground.py
@@ -1,31 +1,32 @@
 from classes import get_prices, __init__, to_dollars
+import decimal
 
 # Asset types and associated variables: 
 mapping = {
     "ETH": {
-        "collateral_factor": 0.8,
-        "bonus": 0.1,
-        "borrow_factor": 1
+        "collateral_factor": decimal.Decimal(0.8),
+        "bonus": decimal.Decimal(0.1),
+        "borrow_factor": decimal.Decimal(1.0)
     },
     "USDC": {
-        "collateral_factor": 0.8,
-        "bonus": 0.1,
-        "borrow_factor": 1
+        "collateral_factor": decimal.Decimal(0.8),
+        "bonus": decimal.Decimal(0.1),
+        "borrow_factor": decimal.Decimal(1)
     },
     "wBTC": {
-        "collateral_factor": 0.7,
-        "bonus": 0.15, 
-        "borrow_factor": 0.91
+        "collateral_factor": decimal.Decimal(0.7),
+        "bonus": decimal.Decimal(0.15), 
+        "borrow_factor": decimal.Decimal(0.91)
     }, 
     "USDT": {
-        "collateral_factor": 0.7,
-        "bonus": 0.1, 
-        "borrow_factor": 0.91
+        "collateral_factor": decimal.Decimal(0.7),
+        "bonus": decimal.Decimal(0.1), 
+        "borrow_factor": decimal.Decimal(0.91)
     }, 
     "DAI": {
-        "collateral_factor": 0.7,
-        "bonus": 0.1, 
-        "borrow_factor": 0.91 
+        "collateral_factor": decimal.Decimal(0.7),
+        "bonus": decimal.Decimal(0.1), 
+        "borrow_factor": (0.91) 
     }
 }
 
@@ -54,7 +55,7 @@ C = collateral_amount * get_prices(collateral_input)
 
 # Collateral Liquidated 
 CL = (loan_input - C * to_dollars(collateral_input) * collateral_factor) / (
-    (to_dollars(collateral_input) * (1-bonus)) - (to_dollars(collateral_input) * collateral_factor) 
+    (to_dollars(collateral_input) * (1 - bonus)) - (to_dollars(collateral_input) * collateral_factor) 
 )
 # Price of Asset in USD
 E = to_dollars(collateral_input) 
@@ -62,12 +63,14 @@ E = to_dollars(collateral_input)
 
 
 # Health: Calculated as collateral value divided by loan value 
-health_0 = (C * E * collateral_factor) / loan_input 
-print("Health", health_0)
+def health():
+    health_0 = (C * E * collateral_factor) / loan_input 
+    print("Health", health_0)
 
 
 # Health AFTER Liquidation 
-health_1 = ((C - CL) * E * collateral_factor) / (loan_input - CL * E * (1 + bonus))
+def health_after_liquidation():
+    health_1 = ((C - CL) * E * collateral_factor) / (loan_input - CL * E * (1 + bonus))
 
 #Simulating 
 for collateral_liquidated in [0.3, 0.35, 0.4, 0.45, 0.5]:
@@ -76,11 +79,3 @@ for collateral_liquidated in [0.3, 0.35, 0.4, 0.45, 0.5]:
     print("liquidating", collateral_liquidated, "\nhealth:", health_2, "\ngain:", gain)
 
 
-
-#health_3 = ((colateral_0 - colateral_liquidated) * ASSET_PRICE_1 * borrow_factor) / (
-    #loan_0 - ASSET_PRICE_1 * (colateral_liquidated * (1 + bonus))
-#)
-#gain = ASSET_PRICE_1 * colateral_liquidated * bonus
-
-#print("calculated liquidation")
-#print("liquidating", colateral_liquidated, "\nhealth:", health_3, "\ngain:", gain)
