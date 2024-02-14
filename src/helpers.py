e403a1666e52d4696ec41d97dc2aa8a9e66ba9ed
lukaspetrasek <petrasek.lks@gmail.com>
2024-02-14 17:07:41 +0000

    add leading zeros

--- src/helpers.py
+++ src/helpers.py
@@ -181,3 +181,11 @@ def save_dataframe(data: pandas.DataFrame, path: str) -> None:
     data.to_parquet(path, index=False, engine = 'fastparquet', compression='gzip')
     src.helpers.upload_file_to_bucket(source_path=path, target_path=path)
     os.remove(path)
+
+
+def add_leading_zeros(hash: str) -> str:
+    '''
+    Converts e.g. `0x436d8d078de345c11493bd91512eae60cd2713e05bcaa0bb9f0cba90358c6e` to  
+    `0x00436d8d078de345c11493bd91512eae60cd2713e05bcaa0bb9f0cba90358c6e`.
+    '''
+    return '0x' + hash[2:].zfill(64)
