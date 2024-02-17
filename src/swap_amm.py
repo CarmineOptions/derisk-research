4c07336db80cd6dd6b46e8a903b9aca7971c8427
lukaspetrasek <petrasek.lks@gmail.com>
2024-02-17 02:27:24 +0000

    implement LP tokens

--- src/swap_amm.py
+++ src/swap_amm.py
@@ -1,15 +1,257 @@
-from typing import Optional
+from typing import Optional, Union
 import dataclasses
 import decimal
 import requests
 
 import src.blockchain_call
+import src.hashstack_v1
 import src.helpers
 import src.settings
 
 
+
+@dataclasses.dataclass
+class JediSwapPoolSettings():
+    symbol: str
+    address: str
+    token_1: str
+    token_2: str
+
+
+JEDISWAP_POOL_SETTINGS: dict[str, JediSwapPoolSettings] = {
+    'JediSwap: DAI/ETH Pool': JediSwapPoolSettings(
+        symbol = 'JediSwap: DAI/ETH Pool',
+        address = '0x07e2a13b40fc1119ec55e0bcf9428eedaa581ab3c924561ad4e955f95da63138',
+        token_1 = '0x00da114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3',
+        token_2 = '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7',
+    ),
+    'JediSwap: DAI/USDC Pool': JediSwapPoolSettings(
+        symbol = 'JediSwap: DAI/USDC Pool',
+        address = '0x00cfd39f5244f7b617418c018204a8a9f9a7f72e71f0ef38f968eeb2a9ca302b',
+        token_1 = '0x00da114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3',
+        token_2 = '0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8',
+    ),
+    'JediSwap: DAI/USDT Pool': JediSwapPoolSettings(
+        symbol = 'JediSwap: DAI/USDT Pool',
+        address = '0x00f0f5b3eed258344152e1f17baf84a2e1b621cd754b625bec169e8595aea767',
+        token_1 = '0x00da114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3',
+        token_2 = '0x068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8',
+    ),
+    'JediSwap: ETH/USDC Pool': JediSwapPoolSettings(
+        symbol = 'JediSwap: ETH/USDC Pool',
+        address = '0x04d0390b777b424e43839cd1e744799f3de6c176c7e32c1812a41dbd9c19db6a',
+        token_1 = '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7',
+        token_2 = '0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8',
+    ),
+    'JediSwap: ETH/USDT Pool': JediSwapPoolSettings(
+        symbol = 'JediSwap: ETH/USDT Pool',
+        address = '0x045e7131d776dddc137e30bdd490b431c7144677e97bf9369f629ed8d3fb7dd6',
+        token_1 = '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7',
+        token_2 = '0x068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8',
+    ),
+    'JediSwap: USDC/USDT Pool': JediSwapPoolSettings(
+        symbol = 'JediSwap: USDC/USDT Pool',
+        address = '0x05801bdad32f343035fb242e98d1e9371ae85bc1543962fedea16c59b35bd19b',
+        token_1 = '0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8',
+        token_2 = '0x068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8',
+    ),
+    'JediSwap: WBTC/ETH Pool': JediSwapPoolSettings(
+        symbol = 'JediSwap: WBTC/ETH Pool',
+        address = '0x0260e98362e0949fefff8b4de85367c035e44f734c9f8069b6ce2075ae86b45c',
+        token_1 = '0x03fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac',
+        token_2 = '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7',
+    ),
+    'JediSwap: WBTC/USDC Pool': JediSwapPoolSettings(
+        symbol = 'JediSwap: WBTC/USDC Pool',
+        address = '0x005a8054e5ca0b277b295a830e53bd71a6a6943b42d0dbb22329437522bc80c8',
+        token_1 = '0x03fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac',
+        token_2 = '0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8',
+    ),
+    'JediSwap: WBTC/USDT Pool': JediSwapPoolSettings(
+        symbol = 'JediSwap: WBTC/USDT Pool',
+        address = '0x044d13ad98a46fd2322ef2637e5e4c292ce8822f47b7cb9a1d581176a801c1a0',
+        token_1 = '0x03fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac',
+        token_2 = '0x068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8',
+    ),
+}
+
+
+@dataclasses.dataclass
+class MySwapPoolSettings():
+    symbol: str
+    address: str
+    myswap_id: int
+    token_1: str
+    token_2: str
+
+
+MYSWAP_POOL_SETTINGS: dict[str, MySwapPoolSettings] = {
+    'mySwap: DAI/ETH Pool': MySwapPoolSettings(
+        symbol = 'mySwap: DAI/ETH Pool',
+        address = '0x010884171baf1914edc28d7afb619b40a4051cfae78a094a55d230f19e944a28',
+        myswap_id = 2,
+        token_1 = '0x00da114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3',
+        token_2 = '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7',
+    ),
+    'mySwap: DAI/USDC Pool': MySwapPoolSettings(
+        symbol = 'mySwap: DAI/USDC Pool',
+        address = '0x010884171baf1914edc28d7afb619b40a4051cfae78a094a55d230f19e944a28',
+        myswap_id = 6,
+        token_1 = '0x00da114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3',
+        token_2 = '0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8',
+    ),
+    'mySwap: ETH/USDC Pool': MySwapPoolSettings(
+        symbol = 'mySwap: ETH/USDC Pool',
+        address = '0x010884171baf1914edc28d7afb619b40a4051cfae78a094a55d230f19e944a28',
+        myswap_id = 1,
+        token_1 = '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7',
+        token_2 = '0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8',
+    ),
+    'mySwap: ETH/USDT Pool': MySwapPoolSettings(
+        symbol = 'mySwap: ETH/USDT Pool',
+        address = '0x010884171baf1914edc28d7afb619b40a4051cfae78a094a55d230f19e944a28',
+        myswap_id = 4,
+        token_1 = '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7',
+        token_2 = '0x068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8',
+    ),
+    'mySwap: USDC/USDT Pool': MySwapPoolSettings(
+        symbol = 'mySwap: USDC/USDT Pool',
+        address = '0x010884171baf1914edc28d7afb619b40a4051cfae78a094a55d230f19e944a28',
+        myswap_id = 5,
+        token_1 = '0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8',
+        token_2 = '0x068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8',
+    ),
+    'mySwap: WBTC/USDC Pool': MySwapPoolSettings(
+        symbol = 'mySwap: WBTC/USDC Pool',
+        address = '0x010884171baf1914edc28d7afb619b40a4051cfae78a094a55d230f19e944a28',
+        myswap_id = 3,
+        token_1 = '0x03fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac',
+        token_2 = '0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8',
+    ),
+}
+
+
+
+class JediSwapPool:
+    """
+    This class implements JediSwap pools where Hashstack V1 users can spend their debt. To properly account for their 
+    token holdings, we collect the total supply of LP tokens and the amounts of both tokens in the pool.
+    """
+
+    def __init__(self, settings: JediSwapPoolSettings) -> None:
+        self.settings: JediSwapPoolSettings = settings
+
+        self.total_lp_supply: Optional[decimal.Decimal] = None
+        self.token_amounts: Optional[src.helpers.TokenValues] = None
+
+    async def get_data(self) -> None:
+        self.total_lp_supply = decimal.Decimal(
+            (
+                await src.blockchain_call.func_call(
+                    addr=self.settings.address, 
+                    selector='totalSupply', 
+                    calldata=[],
+                )
+            )[0]
+        )
+        self.token_amounts = src.helpers.TokenValues()
+        for token in [self.settings.token_1, self.settings.token_2]:
+            self.token_amounts.values[src.helpers.get_symbol(token)] = decimal.Decimal(
+                await src.blockchain_call.balance_of(
+                    token_addr=token, 
+                    holder_addr=self.settings.address,
+                )
+            )
+
+
+class MySwapPool:
+    """
+    This class implements MySwap pools where Hashstack V1 users can spend their debt. To properly account for their 
+    token holdings, we collect the total supply of LP tokens and the amounts of both tokens in the pool.
+    """
+
+    def __init__(self, settings:MySwapPoolSettings) -> None:
+        self.settings: MySwapPoolSettings = settings
+
+        self.total_lp_supply: Optional[decimal.Decimal] = None
+        self.token_amounts: Optional[src.helpers.TokenValues] = None
+
+    async def get_data(self) -> None:
+        self.total_lp_supply = decimal.Decimal(
+            (
+                await src.blockchain_call.func_call(
+                    addr=self.settings.address,
+                    selector="get_total_shares",
+                    calldata=[self.settings.myswap_id],
+                )
+            )[0]
+        )
+        self.token_amounts = src.helpers.TokenValues()
+        # The order of the values returned is: `name`, `token_a_address`, `token_a_reserves`, ``, `token_b_address`,
+        # `token_b_reserves`, ``, `fee_percentage`, `cfmm_type`, `liq_token`.
+        pool = await src.blockchain_call.func_call(
+            addr=self.settings.address,
+            selector="get_pool",
+            calldata=[self.settings.myswap_id],
+        )
+        assert self.settings.token_1 == src.helpers.add_leading_zeros(hex(pool[1]))
+        assert self.settings.token_2 == src.helpers.add_leading_zeros(hex(pool[4]))
+        self.token_amounts.values[src.helpers.get_symbol(self.settings.token_1)] = decimal.Decimal(pool[2])
+        self.token_amounts.values[src.helpers.get_symbol(self.settings.token_2)] = decimal.Decimal(pool[5])
+
+
+class LPTokenPools:
+    """
+    This class initializes all JediSwap and MySwap pools configured in `JEDISWAP_POOL_SETTINGS` and 
+    `MYSWAP_POOL_SETTINGS` and collects the total supply of LP tokens and the amounts of both tokens in each pool.
+    """
+
+    def __init__(self) -> None:
+        self.pools: dict[str, Union[JediSwapPool, MySwapPool]] = {
+            pool_symbol: JediSwapPool(settings=pool_settings)
+            for pool_symbol, pool_settings in JEDISWAP_POOL_SETTINGS.items()
+        }
+        self.pools.update(
+            {
+                pool_symbol: MySwapPool(settings=pool_settings)
+                for pool_symbol, pool_settings in MYSWAP_POOL_SETTINGS.items()
+            }
+        )
+
+    async def get_data(self) -> None:
+        for pool in self.pools.values():
+            await pool.get_data()
+
+
+def _get_lp_token_price(
+    pool: Union[JediSwapPool, MySwapPool],
+    prices: src.helpers.TokenValues,
+) -> decimal.Decimal:
+    token_1 = src.helpers.get_symbol(pool.settings.token_1)
+    token_2 = src.helpers.get_symbol(pool.settings.token_2)
+    token_1_value = (
+        pool.token_amounts.values[token_1]
+        / src.settings.TOKEN_SETTINGS[token_1].decimal_factor
+        * prices.values[token_1]
+    )
+    token_2_value = (
+        pool.token_amounts.values[token_2] 
+        / src.settings.TOKEN_SETTINGS[token_2].decimal_factor
+        * prices.values[token_2]
+    )
+    return (
+        (token_1_value + token_2_value) 
+        / (
+            pool.total_lp_supply 
+            / src.hashstack_v1.HASHSTACK_V1_ADDITIONAL_TOKEN_SETTINGS[pool.settings.symbol].decimal_factor
+        )
+    )
+
+
 class Prices:
+
     def __init__(self):
+        # TODO: Move this mapping to `src.settings.TOKEN_SETTINGS`.
         self.tokens = [
             ("ethereum", "ETH"),
             ("bitcoin", "wBTC"),
@@ -32,33 +274,15 @@ class Prices:
             data = response.json()
             for token in self.tokens:
                 (id, symbol) = token
-                self.prices.values[symbol] = decimal.Decimal(
-                    data[id][self.vs_currency])
+                self.prices.values[symbol] = decimal.Decimal(data[id][self.vs_currency])
         else:
-            raise Exception(
-                f"Failed getting prices, status code {response.status_code}"
-            )
+            raise Exception(f"Failed getting prices, status code = {response.status_code}.")
 
-    def get_by_symbol(self, symbol):
-        symbol = src.helpers.ztoken_to_token(symbol)
-        if symbol in self.prices.values:
-            return self.prices.values[symbol]
-        raise Exception(f"Unknown symbol {symbol}")
-
-    def to_dollars(self, n, symbol):
-        symbol = src.helpers.ztoken_to_token(symbol)
-        try:
-            price = self.prices.values[symbol]
-            decimal_factor = src.settings.TOKEN_SETTINGS[symbol].decimal_factor
-        except:
-            raise Exception(f"Unknown symbol {symbol}")
-        return n / decimal_factor * price
-
-    def to_dollars_pretty(self, n, symbol):
-        v = self.to_dollars(n, symbol)
-        if abs(v) < 0.00001:
-            return "$0"
-        return f"${v:.5f}"
+    async def get_lp_token_prices(self) -> None:
+        lp_token_pools = LPTokenPools()
+        await lp_token_pools.get_data()
+        for lp_token, lp_token_pool in lp_token_pools.pools.items():
+            self.prices.values[lp_token] = _get_lp_token_price(pool=lp_token_pool, prices=self.prices)
 
 
 @dataclasses.dataclass
