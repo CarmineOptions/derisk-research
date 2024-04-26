f99b49bb34e87e29220aae0e76fc827ab620c87a
Cucina <60839899+iamnovichek@users.noreply.github.com>
2024-04-26 10:02:58 +0200

    setup database connection

---
+++ web_app/database/database.py
@@ -0,0 +1,33 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv("../")
+
+DB_USER = os.environ.get("DB_USER", "")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "")
+DB_HOST = os.environ.get("DB_HOST", "")
+DB_PORT = os.environ.get("DB_PORT", 5432)
+DB_NAME = os.environ.get("DB_NAME", "")
+
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+Base.metadata.create_all(bind=engine)
+
+
+def get_database() -> SessionLocal:
+    database = SessionLocal()
+    try:
+        yield database
+    finally:
+        database.close()
