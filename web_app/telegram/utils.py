351b17808d56d4aba17b213827c17ab4e297e8a2
teri-anric <2005ahi2005@gmail.com>
2024-04-30 21:05:49 +0300

    change int ident to uuid

--- web_app/telegram/utils.py
+++ web_app/telegram/utils.py
@@ -1,11 +1,12 @@
 from asyncio.queues import Queue
+from uuid import UUID
 
 from aiogram.utils.deep_linking import create_deep_link
 
 from .bot import bot
 
 
-async def get_subscription_link(ident: int):
+async def get_subscription_link(ident: UUID):
     me = await bot.me()
     return create_deep_link(username=me.username, link_type="start", payload=str(ident), encode=True)
 
