8e37c13c2f322e7b7c484501e1e8453b37042947
teri-anric <2005ahi2005@gmail.com>
2024-05-07 23:37:50 +0300

    add telegram log table to database

--- web_app/database/models.py
+++ web_app/database/models.py
@@ -39,3 +39,21 @@ class NotificationData(Base):
     ip_address = Column(IPAddressType, nullable=False)
     health_ratio_level = Column(Float, nullable=False)
     protocol_id = Column(ChoiceType(ProtocolIDs, impl=String()), nullable=False)
+
+
+class TelegramLog(Base):
+    """
+    Represents a log entry for Telegram notifications.
+
+    :ivar sent_at: The timestamp indicating when the message was sent.
+    :ivar notification_data_id: The UUID identifying the notification data associated with this log entry.
+    :ivar is_succesfully: A boolean indicating whether the message was sent successfully or not.
+    :ivar message: The content of the send message being logged.
+    """
+
+    __tablename__ = "telegram_log"
+
+    sent_at = Column(DateTime, default=datetime.now(), nullable=False)
+    notification_data_id = Column(ForeignKey(NotificationData.id), nullable=True)
+    is_succesfully = Column(Boolean, nullable=False)
+    message = Column(String, server_default="", default="", nullable=False)
