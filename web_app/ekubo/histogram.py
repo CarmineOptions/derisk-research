fdcbb481f52ed6a93554b6629fdf759d7bf4b997
djeck1432 <oleh.kostashchuk@gmail.com>
2024-05-23 20:17:22 +0200

    update histogram code

--- web_app/ekubo/histogram.py
+++ web_app/ekubo/histogram.py
@@ -2,9 +2,8 @@ import matplotlib.pyplot as plt
 from web_app.ekubo.main import EkuboOrderBook
 
 TOKEN_A = "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7"  # ETH
-TOKEN_B = (
-    "0x53c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8"  # USDC
-)
+TOKEN_B = "0x53c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8"  # USDC
+
 
 def fetch_order_book_and_current_price(token_a: str, token_b: str) -> tuple:
     """
@@ -21,33 +20,41 @@ def fetch_order_book_and_current_price(token_a: str, token_b: str) -> tuple:
 if __name__ == "__main__":
     data, current_price = fetch_order_book_and_current_price(TOKEN_A, TOKEN_B)
     # Extract data for plotting and convert to float
-    ask_prices, ask_quantities = zip(*data['asks'])
-    bid_prices, bid_quantities = zip(*data['bids'])
+    ask_prices, ask_quantities = zip(*data["asks"])
+    bid_prices, bid_quantities = zip(*data["bids"])
 
     ask_prices = [float(price) for price in ask_prices]
     ask_quantities = [float(quantity) for quantity in ask_quantities]
     bid_prices = [float(price) for price in bid_prices]
     bid_quantities = [float(quantity) for quantity in bid_quantities]
 
-    # Create the figure and axis
+    # Convert current_price to float
+    current_price = float(current_price)
+
     fig, ax = plt.subplots()
+    # Add current price line
+    ax.barh(
+        [current_price],
+        max((max(bid_quantities), max(ask_quantities))),
+        color="black",
+        height=20,
+        label="Current Price",
+    )
 
     # Plot asks and bids
-    ax.barh(ask_prices, ask_quantities, color='red', label='Asks')
-    ax.barh(bid_prices, bid_quantities, color='green', label='Bids')
+    ax.barh(ask_prices, ask_quantities, color="red", label="Asks", height=5)
+    ax.barh(bid_prices, bid_quantities, color="green", label="Bids", height=5)
 
-    # Highlight the current price
-    ax.axhline(current_price, color='black', linestyle='--', linewidth=4, label='Current Price')
+    # Set y-axis limits to include current_price
+    min_price = min(min(bid_prices), min(ask_prices), current_price)
+    max_price = max(max(bid_prices), max(ask_prices), current_price)
+    ax.set_ylim(min_price - 100, max_price + 100)  # Adding some buffer
 
     # Labels and title
-    ax.set_xlabel('Quantity (ETH)')
-    ax.set_ylabel('Price (USDT)')
-    ax.set_title('Order Book')
+    ax.set_xlabel("Quantity (ETH)")
+    ax.set_ylabel("Price (USDC)")
+    ax.set_title("Order Book")
     ax.legend()
-
-    #  Adjust limits to make sure the current price line is visible
-    ax.set_xlim(0, max(max(ask_quantities), max(bid_quantities)) * 1.1)
-    ax.set_ylim(min(bid_prices) - 10, max(ask_prices) + 10)
 
     # Show the plot
     plt.show()
