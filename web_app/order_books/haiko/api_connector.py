df496f8d22282ed1e4d93b1964e777c4f38de2c6
axeratos <nikitalukianitcares@gmail.com>
2024-05-25 15:43:21 +0200

    Blast API connector created

--- web_app/order_books/haiko/api_connector.py
+++ web_app/order_books/haiko/api_connector.py
@@ -15,3 +15,22 @@ class HaikoAPIConnector(AbstractionAPIConnector):
             raise ValueError("Existing only parameter must be a bool")
         endpoint = f"/tokens?network=mainnet&existingOnly={existing_only}"
         return cls.send_get_request(endpoint)
+
+
+class HaikoBlastAPIConnector(AbstractionAPIConnector):
+    API_URL = "https://starknet-mainnet.blastapi.io"
+    PROJECT_ID = "a419bd5a-ec9e-40a7-93a4-d16467fb79b3"
+
+    @classmethod
+    def _post_request_builder(cls, call_method: str, params: dict):
+        if not isinstance(call_method, str) or not isinstance(params, dict):
+            raise ValueError("Call method must be a string and params must be a dict")
+        body = {
+            "jsonrpc": "2.0", "method": call_method, "params": params, "id": 0
+        }
+        endpoint = f"/{cls.PROJECT_ID}"
+        return cls.send_post_request(endpoint, json=body)
+
+    @classmethod
+    def get_block_info(cls) -> dict:
+        return cls._post_request_builder("starknet_getBlockWithTxHashes", params={"block_id": "latest"})
