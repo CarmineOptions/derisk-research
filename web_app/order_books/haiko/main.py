d67ea3939c7955174d748daabd384d26d944dae3
axeratos <nikitalukianitcares@gmail.com>
2024-05-25 15:44:44 +0200

    Checks added, extra info from blast api, bids and asks sorting

--- web_app/order_books/haiko/main.py
+++ web_app/order_books/haiko/main.py
@@ -5,7 +5,7 @@ from itertools import zip_longest
 
 from src.blockchain_call import func_call
 from web_app.order_books.abstractions import OrderBookBase
-from web_app.order_books.haiko.api_connector import HaikoAPIConnector
+from web_app.order_books.haiko.api_connector import HaikoAPIConnector, HaikoBlastAPIConnector
 
 HAIKO_MARKET_MANAGER_ADDRESS = "0x0038925b0bcf4dce081042ca26a96300d9e181b910328db54a6c89e5451503f5"
 HAIKO_MAX_TICK = 7906205
@@ -16,11 +16,13 @@ class HaikoOrderBook(OrderBookBase):
 
     def __init__(self, token_a, token_b):
         super().__init__(token_a, token_b)
-        self.connector = HaikoAPIConnector()
+        self.current_price = Decimal("0")
+        self.haiko_connector = HaikoAPIConnector()
+        self.blast_connector = HaikoBlastAPIConnector()
         self._check_tokens_supported()
 
     def _check_tokens_supported(self) -> None:
-        supported_tokens = self.connector.get_supported_tokens()
+        supported_tokens = self.haiko_connector.get_supported_tokens()
         if isinstance(supported_tokens, dict) and supported_tokens.get("error"):
             raise RuntimeError(f"Unexpected error from API: {supported_tokens}")
         supported_tokens_filtered = [
@@ -51,23 +53,38 @@ class HaikoOrderBook(OrderBookBase):
         self.tick_current_price = self.tick_to_price(current_tick)
 
     def sort_asks_bids(self):
-        pass
+        self.asks.sort(key=lambda ask: ask[0])
+        self.bids.sort(key=lambda bid: bid[0], reverse=True)
 
     async def fetch_price_and_liquidity(self) -> None:
-        all_markets_data = self.connector.get_token_markets()
+        all_markets_data = self.haiko_connector.get_token_markets()
         if isinstance(all_markets_data, dict) and all_markets_data.get("error"):
-            raise RuntimeError(f"Unexpected error from API: {all_markets_data}")
+            raise RuntimeError(f"Unexpected error from Haiko API: {all_markets_data}")
 
         tokens_markets = self._filter_markets_data(all_markets_data)
+        latest_block_info = self.blast_connector.get_block_info()
+        if latest_block_info.get("error"):
+            raise RuntimeError(f"Blast-api returned an error: {latest_block_info}")
         if not tokens_markets:
             raise RuntimeError("Markets for this pair isn't available for now")
+
+        self.block = latest_block_info["result"]["block_number"]
+        self.timestamp = latest_block_info["result"]["timestamp"]
+
         for market in tokens_markets:
-            market_id = market["market_id"]
+            market_id = market["marketId"]
             liquidity = await func_call(HAIKO_MARKET_MANAGER_ADDRESS, "liquidity", [market_id])
+            if not liquidity:
+                continue
+            liquidity = Decimal(liquidity[0])
             market_ticks_liquidity = await self.get_market_ticks_liquidity(market_id)
-            if not market_ticks_liquidity:
+            if not market_ticks_liquidity or market_ticks_liquidity[0] == 0:
                 continue
             self._calculate_order_book(market_ticks_liquidity, liquidity, market["currLimit"])
+
+        self.sort_asks_bids()
+
+        self.current_price = max(tokens_markets, key=lambda x: Decimal(x["tvl"]))["currPrice"]
 
     def _calculate_order_book(
             self,
@@ -107,7 +124,7 @@ class HaikoOrderBook(OrderBookBase):
                 )
 
     def divide_ticks_on_bids_asks(self, ticks_info: list[tuple]) -> tuple[list, list]:
-        current_price_formatted = self.tick_current_price * (10 ** self.token_a_decimal + self.token_b_decimal)
+        current_price_formatted = self.tick_current_price
         asks_data, bids_data = [], []
         for group in ticks_info:
             signed_tick = Decimal(group[0] - HAIKO_MAX_TICK)
@@ -127,28 +144,16 @@ class HaikoOrderBook(OrderBookBase):
         return Decimal("1.00001") ** tick * (10 ** (self.token_a_decimal - self.token_b_decimal))
 
     @staticmethod
-    def group_ticks_info(ticks_info, n, fillvalue=None):
-        if n == 0:
-            return 0
-        if len(ticks_info) % n != 0:
+    def group_ticks_info(ticks_info, n):
+        if n == 0 or len(ticks_info) % n != 0:
             raise ValueError("Ticks info list should be dividable by n")
         args = [iter(ticks_info)] * n
-        return zip_longest(*args, fillvalue=fillvalue)
+        return zip_longest(*args, fillvalue=0)
 
 
 if __name__ == '__main__':
-    token_0 = "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7"  # ETH
-    token_1 = "0x53c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8"  # USDC
+    token_0 = "0x53c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8"  # USDC
+    token_1 = "0x68f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8"  # USDT
     order_book = HaikoOrderBook(token_0, token_1)
-    print(order_book.tick_to_price(Decimal(-1963856)))
-    # order_book.fetch_price_and_liquidity()
-    res = asyncio.run(
-        order_book.get_market_ticks_liquidity("0x3c62f0152e3b7fdc0144c7a06af97e2e007008d67321074fc336798975ecbeb")
-    )
-    res = list(HaikoOrderBook.group_ticks_info(res[1:], 5))
-    # print((Decimal("1.00001") ** 5953400) * 10 ** 12)
-    # print(Decimal(0.0003911729500584983))
-    # print(Decimal(3911729500584983102980878) / (10 ** 28))
-    print(Decimal(28773339936904503489).sqrt() / 10**18)
-    print(Decimal(34105150426706005793))
-    print(Decimal(5942350) - Decimal(7906205))
+    asyncio.run(order_book.fetch_price_and_liquidity())
+    print(order_book.get_order_book(), "\n")
