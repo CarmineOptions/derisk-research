2829fa43be8d72238ee573bff2a3c6e99c17bd37
teri-anric <2005ahi2005@gmail.com>
2024-05-26 17:14:43 +0300

    added to bot menu `menu` command

--- web_app/telegram/__main__.py
+++ web_app/telegram/__main__.py
@@ -1,5 +1,7 @@
 import asyncio
 
+from aiogram.types import BotCommand, BotCommandScopeDefault
+
 from database.database import SessionLocal
 from . import dp, bot
 from .middleware import DatabaseMiddleware
@@ -8,8 +10,12 @@ from .middleware import DatabaseMiddleware
 async def bot_start_polling():
     """
     Start the bot polling loop
-    (added database middleware if is outside Flask Api server)
+    (added database middleware and bot commands if is outside Flask Api server)
     """
+    await bot.set_my_commands(
+        [BotCommand(command="menu", description="Show bot menu")],
+        scope=BotCommandScopeDefault,
+    )
     dp.update.middleware(DatabaseMiddleware(SessionLocal))
     await dp.start_polling(bot)
 
