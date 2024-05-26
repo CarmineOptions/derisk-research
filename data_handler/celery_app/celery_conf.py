ee926556c7d82268ac420616e971a86f85786476
djeck1432 <oleh.kostashchuk@gmail.com>
2024-05-26 21:46:00 +0200

    comment also in celery app

--- data_handler/celery_app/celery_conf.py
+++ data_handler/celery_app/celery_conf.py
@@ -41,15 +41,15 @@ app.conf.beat_schedule = {
     #     'schedule': crontab(minute=f'*/{CRONTAB_TIME}'),
     # },
 }
+from celery_app.tasks import (
+    # run_loan_states_computation_for_hashtack_v0,
+    # run_loan_states_computation_for_hashtack_v1,
+    # run_loan_states_computation_for_nostra_alpha,
+    # run_loan_states_computation_for_nostra_mainnet,
+    run_loan_states_computation_for_zklend,
+)
 
 app.autodiscover_tasks(["celery_app.tasks"])
 
-from celery_app.tasks import (
-    run_loan_states_computation_for_hashtack_v0,
-    run_loan_states_computation_for_hashtack_v1,
-    run_loan_states_computation_for_nostra_alpha,
-    run_loan_states_computation_for_nostra_mainnet,
-    run_loan_states_computation_for_zklend,
-)
 
 
