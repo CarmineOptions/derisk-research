309bb483e14163fae92cffe8c072f109b465ba82
djeck1432 <oleh.kostashchuk@gmail.com>
2024-05-27 22:52:02 +0200

    first run zklend

--- data_handler/celery_app/celery_conf.py
+++ data_handler/celery_app/celery_conf.py
@@ -17,7 +17,7 @@ app = Celery(
     backend=f"redis://{REDIS_HOST}:{REDIS_PORT}/0",
 )
 
-CRONTAB_TIME = os.environ.get("CRONTAB_TIME", "1")
+CRONTAB_TIME = os.environ.get("CRONTAB_TIME", "5")
 
 app.conf.beat_schedule = {
     # f'run_loan_states_computation_for_hashtack_v0_every_{CRONTAB_TIME}_mins': {
@@ -28,25 +28,25 @@ app.conf.beat_schedule = {
     #     'task': 'run_loan_states_computation_for_hashtack_v1',
     #     'schedule': crontab(minute=f'*/{CRONTAB_TIME}'),
     # },
-    # f'run_loan_states_computation_for_zklend_every_{CRONTAB_TIME}_mins': {
-    #     'task': 'run_loan_states_computation_for_zklend',
-    #     'schedule': crontab(minute=f'*/{CRONTAB_TIME}'),
-    # },
-    f'run_loan_states_computation_for_nostra_alpha_every_{CRONTAB_TIME}_mins': {
-        'task': 'run_loan_states_computation_for_nostra_alpha',
+    f'run_loan_states_computation_for_zklend_every_{CRONTAB_TIME}_mins': {
+        'task': 'run_loan_states_computation_for_zklend',
         'schedule': crontab(minute=f'*/{CRONTAB_TIME}'),
     },
+    # f'run_loan_states_computation_for_nostra_alpha_every_{CRONTAB_TIME}_mins': {
+    #     'task': 'run_loan_states_computation_for_nostra_alpha',
+    #     'schedule': crontab(minute=f'*/{CRONTAB_TIME}'),
+    # },
     # f'run_loan_states_computation_for_nostra_mainnet_every_{CRONTAB_TIME}_mins': {
     #     'task': 'run_loan_states_computation_for_nostra_mainnet',
     #     'schedule': crontab(minute=f'*/{CRONTAB_TIME}'),
     # },
 }
 from celery_app.tasks import (
-    # run_loan_states_computation_for_hashtack_v0,
-    # run_loan_states_computation_for_hashtack_v1,
-    run_loan_states_computation_for_nostra_alpha,
-    # run_loan_states_computation_for_nostra_mainnet,
-    # run_loan_states_computation_for_zklend,
+#     # run_loan_states_computation_for_hashtack_v0,
+#     # run_loan_states_computation_for_hashtack_v1,
+#     # run_loan_states_computation_for_nostra_alpha,
+#     # run_loan_states_computation_for_nostra_mainnet,
+    run_loan_states_computation_for_zklend
 )
 
 app.autodiscover_tasks(["celery_app.tasks"])
