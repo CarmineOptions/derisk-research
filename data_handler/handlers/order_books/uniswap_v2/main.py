8f1775d63fa496269d76aa15effd1fcb8e296017
axeratos <nikitalukianitcares@gmail.com>
2024-06-03 19:44:30 +0200

    Core order book logic and some checks added

--- data_handler/handlers/order_books/uniswap_v2/main.py
+++ data_handler/handlers/order_books/uniswap_v2/main.py
@@ -1,7 +1,8 @@
 from decimal import Decimal
 import asyncio
+from typing import Iterable
 
-from handlers.helpers import get_range
+from handlers.helpers import get_range, get_collateral_token_range
 from handlers.order_books.abstractions import OrderBookBase
 from handlers.order_books.uniswap_v2.swap_amm import MySwapPool, SwapAmm
 
@@ -13,49 +14,51 @@ class UniswapV2OrderBook(OrderBookBase):
         super().__init__(token_a, token_b)
         self.token_a = token_a
         self.token_b = token_b
-        self.pool = None
-        self.swap_amm = SwapAmm()
-        # setting = MYSWAP_POOL_SETTINGS[f"mySwap: {self.token_a}/{self.token_b} Pool"]
-        # self.pool = MySwapPool(setting)
+        self._pool = None
+        self._swap_amm = SwapAmm()
 
-    async def async_fetch_price_and_liquidity(self) -> None:
-        # await self.pool.get_data()
-        await self.swap_amm.init()
-        self.pool = self.swap_amm.pools[self.swap_amm.tokens_to_id(self.token_a, self.token_b)]
-        if isinstance(self.pool, MySwapPool):
-            await self.pool.get_data()
+    def _set_pool(self) -> None:
+        self._pool = self._swap_amm.pools[self._swap_amm.tokens_to_id(self.token_a, self.token_b)]
+
+    async def _async_fetch_price_and_liquidity(self) -> None:
+        await self._swap_amm.init()
+        self._set_pool()
         self._calculate_order_book()
 
     def fetch_price_and_liquidity(self) -> None:
         loop = asyncio.get_event_loop()
-        # Check if there is a running event loop (e.g., in Jupyter notebook or other async environments)
         if loop.is_running():
-            # If there is already a running event loop, create a new one and run it
             new_loop = asyncio.new_event_loop()
             asyncio.set_event_loop(new_loop)
-            new_loop.run_until_complete(self.async_fetch_price_and_liquidity())
+            new_loop.run_until_complete(self._async_fetch_price_and_liquidity())
             asyncio.set_event_loop(loop)
         else:
-            # If no running event loop, use the current one
-            loop.run_until_complete(self.async_fetch_price_and_liquidity())
+            loop.run_until_complete(self._async_fetch_price_and_liquidity())
+
+    def get_prices_range(self, current_price: Decimal) -> Iterable[Decimal]:
+        collateral_tokens = ("ETH", "wBTC", "STRK")
+        if self.token_a in collateral_tokens:
+            return get_collateral_token_range(self.token_a, current_price)
+        return get_range(Decimal(0), current_price * Decimal("1.3"), Decimal(current_price / 100))
 
     def _calculate_order_book(self) -> None:
-        # TODO: fix data fetching for another pairs
-        if not self.pool:
-            raise RuntimeError("Pool is not initialized")
-        token_a_reserves = self.pool.tokens[0].balance_converted
-        token_b_reserves = self.pool.tokens[1].balance_converted
-        current_price = Decimal(token_b_reserves / token_a_reserves)
-        # total_liquidity = self.pool.total_lp_supply
-        # TODO: add checks for collateral token
-        bids_range = get_range(Decimal(0), current_price, Decimal(current_price / 100))
-        asks_range = get_range(current_price, current_price * Decimal("1.3"), Decimal(current_price / 100))
-        for price in bids_range:
-            liquidity = self.pool.supply_at_price(price)
-            self.bids.append((price, liquidity))
-        for price in asks_range:
-            liquidity = self.pool.supply_at_price(price)
-            self.asks.append((price, liquidity))
+        token_a_reserves = Decimal(self._pool.tokens[0].balance_converted)
+        token_b_reserves = Decimal(self._pool.tokens[1].balance_converted)
+        if token_a_reserves == 0 or token_b_reserves == 0:
+            raise RuntimeError("Reserves can't be zero")
+        current_price = token_b_reserves / token_a_reserves
+        prices_range = self.get_prices_range(current_price)
+        self.add_quantities_data(prices_range, current_price)
+
+    def add_quantities_data(self, prices_range: Iterable[Decimal], current_price: Decimal) -> None:
+        if not prices_range or current_price == 0:
+            raise ValueError("Provide valid prices range and current price for analysis.")
+        for price in prices_range:
+            supply = self._pool.supply_at_price(price)
+            if price < current_price:
+                self.bids.append((price, supply))
+            else:
+                self.asks.append((price, supply))
 
     def calculate_liquidity_amount(self, tick: Decimal, liquidity_pair_total: Decimal) -> Decimal:
         sqrt_ratio = self.get_sqrt_ratio(tick)
