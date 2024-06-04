bc1e4c29c2cdc13407945fcce5891905bcec461d
Emmanuel Agüero <54730752+EmmanuelAR@users.noreply.github.com>
2024-06-03 22:59:16 -0600

    Remove static ZKLEND_SPECIFIC_TOKEN_SETTINGS constant for each file , instead of that  import  the ZKLEND_SPECIFIC_TOKEN_SETTINGS from fetch_zklend_specific_token_settings.py file

--- src/zklend.py
+++ src/zklend.py
@@ -9,7 +9,7 @@ import pandas
 import src.helpers
 import src.settings
 import src.state
-
+from data_handler.handlers.loan_states.zklend.fetch_zklend_specific_token_settings import ZKLEND_SPECIFIC_TOKEN_SETTINGS
 
 
 ADDRESS: str = "0x04c0a5193d58f74fbace4b74dcf65481e734ed1714121bdc571da345540efa05"
@@ -31,58 +31,6 @@ class TokenSettings(ZkLendSpecificTokenSettings, src.settings.TokenSettings):
     pass
 
 
-ZKLEND_SPECIFIC_TOKEN_SETTINGS: dict[str, ZkLendSpecificTokenSettings] = {
-    "ETH": ZkLendSpecificTokenSettings(
-        collateral_factor=decimal.Decimal("0.80"),
-        debt_factor=decimal.Decimal("1"),
-        liquidation_bonus=decimal.Decimal("0.10"),
-        protocol_token_address='0x01b5bd713e72fdc5d63ffd83762f81297f6175a5e0a4771cdadbc1dd5fe72cb1',
-    ),
-    "wBTC": ZkLendSpecificTokenSettings(
-        collateral_factor=decimal.Decimal("0.70"),
-        debt_factor=decimal.Decimal("1"),
-        liquidation_bonus=decimal.Decimal("0.15"),
-        protocol_token_address='0x02b9ea3acdb23da566cee8e8beae3125a1458e720dea68c4a9a7a2d8eb5bbb4a',
-    ),
-    "USDC": ZkLendSpecificTokenSettings(
-        collateral_factor=decimal.Decimal("0.80"),
-        debt_factor=decimal.Decimal("1"),
-        liquidation_bonus=decimal.Decimal("0.10"),
-        protocol_token_address='0x047ad51726d891f972e74e4ad858a261b43869f7126ce7436ee0b2529a98f486',
-    ),
-    "DAI": ZkLendSpecificTokenSettings(
-        collateral_factor=decimal.Decimal("0.70"),
-        debt_factor=decimal.Decimal("1"),
-        liquidation_bonus=decimal.Decimal("0.10"),
-        protocol_token_address='0x062fa7afe1ca2992f8d8015385a279f49fad36299754fb1e9866f4f052289376',
-    ),
-    "USDT": ZkLendSpecificTokenSettings(
-        collateral_factor=decimal.Decimal("0.80"),
-        debt_factor=decimal.Decimal("1"),
-        liquidation_bonus=decimal.Decimal("0.10"),
-        protocol_token_address='0x00811d8da5dc8a2206ea7fd0b28627c2d77280a515126e62baa4d78e22714c4a',
-    ),
-    "wstETH": ZkLendSpecificTokenSettings(
-        collateral_factor=decimal.Decimal("0.80"),
-        debt_factor=decimal.Decimal("1"),
-        liquidation_bonus=decimal.Decimal("0.10"),
-        protocol_token_address='0x0536aa7e01ecc0235ca3e29da7b5ad5b12cb881e29034d87a4290edbb20b7c28',
-    ),
-    # TODO: Add LORDS.
-    "LORDS": ZkLendSpecificTokenSettings(
-        collateral_factor=decimal.Decimal("1"),
-        debt_factor=decimal.Decimal("1"),
-        liquidation_bonus=decimal.Decimal("0"),
-        protocol_token_address='',
-    ),
-    # TODO: Update STRK settings.
-    "STRK": ZkLendSpecificTokenSettings(
-        collateral_factor=decimal.Decimal("0.50"),
-        debt_factor=decimal.Decimal("1"),
-        liquidation_bonus=decimal.Decimal("0.15"),
-        protocol_token_address='0x06d8fa671ef84f791b7f601fa79fea8f6ceb70b5fa84189e3159d532162efc21',
-    ),
-}
 TOKEN_SETTINGS: dict[str, TokenSettings] = {
     token: TokenSettings(
         symbol=src.settings.TOKEN_SETTINGS[token].symbol,
