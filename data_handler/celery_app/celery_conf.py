cae00e4f18f119e4057ab0151b8462f8b5ab0f96
axeratos <nikitalukianitcares@gmail.com>
2024-06-04 17:47:24 +0200

    Task name for uniswap v2 corrected

--- data_handler/celery_app/celery_conf.py
+++ data_handler/celery_app/celery_conf.py
@@ -47,7 +47,7 @@ app.conf.beat_schedule = {
         'task': 'run_liquidable_debt_computation_for_zklend',
         'schedule': crontab(minute=f'*/{CRONTAB_TIME}'),
     },
-    "uniswap-v2-order-book": {
+    "constant_product_market_makers_order_book": {
         "task": "uniswap_v2_order_book",
         "schedule": ORDER_BOOK_TIME_INTERVAL,
     },
@@ -59,7 +59,8 @@ from celery_app.tasks import (
     run_loan_states_computation_for_nostra_alpha,
     # run_loan_states_computation_for_nostra_mainnet,
     # run_loan_states_computation_for_zklend,
-    run_liquidable_debt_computation_for_zklend
+    run_liquidable_debt_computation_for_zklend,
+    uniswap_v2_order_book,
 )
 
 app.autodiscover_tasks(["celery_app.tasks"])
