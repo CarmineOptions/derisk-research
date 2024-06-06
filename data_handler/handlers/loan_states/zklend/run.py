af7be24ffc2008f6b9f1a5488a2d2116eb6d8379
djeck1432 <oleh.kostashchuk@gmail.com>
2024-06-06 09:02:07 +0200

    rework Zklend loan states computation

--- data_handler/handlers/loan_states/zklend/run.py
+++ data_handler/handlers/loan_states/zklend/run.py
@@ -16,6 +16,37 @@ class ZkLendLoanStateComputation(LoanStateComputationBase):
 
     PROTOCOL_TYPE = ProtocolIDs.ZKLEND.value
     PROTOCOL_ADDRESSES = ProtocolAddresses().ZKLEND_MARKET_ADDRESSES
+    INTEREST_RATES_KEYS = [
+        "AccumulatorsSync",
+        "zklend::market::Market::AccumulatorsSync",
+    ]
+
+    def process_interest_rate_event(
+        self, zklend_state: ZkLendState, event: pd.Series
+    ) -> None:
+        """
+        Processes an interest rate event.
+
+        :param zklend_state: The zkLend state object.
+        :type zklend_state: ZkLendState
+        :param event: The data of the event.
+        :type event: pd.Series
+        """
+        zklend_state.process_accumulators_sync_event(event)
+        self.interest_rate_result.append(
+            {
+                "block": event["block_number"],
+                "timestamp": event["timestamp"],
+                "debt": {
+                    token: float(amount)
+                    for token, amount in zklend_state.debt_interest_rate_models.values.items()
+                },
+                "collateral": {
+                    token: float(amount)
+                    for token, amount in zklend_state.collateral_interest_rate_models.values.items()
+                },
+            }
+        )
 
     def process_data(self, data: list[dict]) -> pd.DataFrame:
         """
@@ -31,9 +62,12 @@ class ZkLendLoanStateComputation(LoanStateComputationBase):
         events_mapping = zklend_state.EVENTS_METHODS_MAPPING
         # Init DataFrame
         df = pd.DataFrame(data)
+
         # Filter out events that are not in the mapping
         df_filtered = df[df["key_name"].isin(events_mapping.keys())]
-        for index, row in df_filtered.iterrows():
+        sorted_df = df_filtered.sort_values(by=["block_number", "id"])
+
+        for index, row in sorted_df.iterrows():
             method_name = events_mapping.get(row["key_name"], "") or ""
             self.process_event(zklend_state, method_name, row)
 
@@ -43,6 +77,41 @@ class ZkLendLoanStateComputation(LoanStateComputationBase):
             for loan in zklend_state.loan_entities.values()
         ]
         return result_df
+
+    def process_event(
+        self, instance_state: ZkLendState, method_name: str, event: pd.Series
+    ) -> None:
+        """
+        Processes an event based on the method name and the event data.
+
+        Updates the last block processed to ensure data consistency
+        and calls the appropriate method to handle the event.
+
+        :param instance_state: The instance of the state class to call the method on.
+        :type instance_state: object
+        :param method_name: The name of the method to call for processing the event.
+        :param event: The event data as a pandas Series.
+        """
+        try:
+            block_number = event.get("block_number")
+            # For each block number, process the interest rate event
+            if (
+                self.last_block != block_number
+                and event["key_name"] in self.INTEREST_RATES_KEYS
+            ):
+                self.process_interest_rate_event(instance_state, event)
+
+            if block_number and block_number >= self.last_block:
+                self.last_block = block_number
+                method = getattr(instance_state, method_name, None)
+                if method:
+                    method(event)
+                else:
+                    logger.info(
+                        f"No method named {method_name} found for processing event."
+                    )
+        except Exception as e:
+            logger.exception(f"Failed to process event due to an error: {e}")
 
 
 def run_loan_states_computation_for_zklend() -> None:
@@ -60,3 +129,7 @@ def run_loan_states_computation_for_zklend() -> None:
         "Finished zkLend loan state computation, Time taken: %s seconds",
         monotonic() - start,
     )
+
+
+if __name__ == "__main__":
+    run_loan_states_computation_for_zklend()
