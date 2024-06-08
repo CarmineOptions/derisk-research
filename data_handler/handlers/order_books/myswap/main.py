37061f08d5b11d46e4e6f7b704dd24c0cadf5d08
axeratos <nikitalukianitcares@gmail.com>
2024-06-09 01:10:09 +0200

    Core raw logic for myswap

--- data_handler/handlers/order_books/myswap/main.py
+++ data_handler/handlers/order_books/myswap/main.py
@@ -11,14 +11,19 @@ from handlers.order_books.myswap.api_connector import MySwapAPIConnector
 
 
 MYSWAP_CL_MM_ADDRESS = "0x01114c7103e12c2b2ecbd3a2472ba9c48ddcbf702b1c242dd570057e26212111"
+MAX_MYSWAP_TICK = Decimal("1774532")
 
 
 class MySwapOrderBook(OrderBookBase):
     DEX = "MySwap"
+    MIN_PRICE_RANGE = Decimal("0.1")
+    MAX_PRICE_RANGE = Decimal("1.3")
 
-    def __init__(self, token_a: str, token_b: str):
+    def __init__(self, token_a: str, token_b: str, apply_filtering: bool = False):
         super().__init__(token_a, token_b)
         self.connector = MySwapAPIConnector()
+        self.apply_filtering = apply_filtering
+        self._decimals_diff = Decimal(10 ** (self.token_a_decimal - self.token_b_decimal))
 
     def _read_liquidity_data(self, pool_id: str) -> pd.DataFrame:
         url = f"https://myswap-cl-charts.s3.amazonaws.com/data/pools/{pool_id}/liqmap.json.gz"
@@ -39,16 +44,111 @@ class MySwapOrderBook(OrderBookBase):
             all_pools["pools"]
         ))
 
+    def _get_ticks_range(self) -> tuple[Decimal, Decimal]:
+        price_range = self.calculate_price_range()
+        return self._price_to_tick(price_range[0]), self._price_to_tick(price_range[1])
+
+    def _price_to_tick(self, price: Decimal) -> Decimal:
+        signed_tick = round(
+            Decimal(math.log(price / (Decimal(2 ** 128) * Decimal(10 ** 12)))) / Decimal(math.log(Decimal("1.0001")))
+        )
+        return Decimal(signed_tick) + MAX_MYSWAP_TICK
+
     async def _calculate_order_book(self, pool_id: str) -> None:
+        if pool_id != "0x4fd41eced6b9425ca6628fcd358002a895513a7d0ef383de1b1e40cd6f54a27":
+            return
         data = self._read_liquidity_data(pool_id)
         current_tick = await func_call(MYSWAP_CL_MM_ADDRESS, "current_tick", [pool_id])
-        data_filtered = data.loc[data["liq"] > 0]
+        current_tick = current_tick[0]
+        self.current_price = self.tick_to_price(current_tick)
+        min_tick, max_tick = self._get_ticks_range()
+        data = data.loc[(min_tick < data["tick"]) & (data["tick"] <= max_tick)]
+        asks, bids = data[data["tick"] >= current_tick], data[data["tick"] < current_tick]
+        self.add_bids(bids, (min_tick, max_tick))
+        self.add_asks(asks, bids.iloc[0]["liq"], (min_tick, max_tick))
+        tvl = (sum([bid[1] for bid in self.bids]) * 3676) + (sum([ask[1] for ask in self.asks]) * 3676)
+        print()
 
-    def calculate_price_range(self) -> tuple:
-        pass
+    def add_asks(self, pool_asks: pd.DataFrame, pool_liquidity: Decimal, price_range: tuple[Decimal, Decimal]) -> None:
+        if pool_asks.empty:
+            return
+        local_asks = []
+        prev_tick = Decimal(pool_asks.iloc[0]['tick'].item())
+        prev_price = self.tick_to_price(prev_tick)
+        y = self._get_token_amount(
+            pool_liquidity,
+            self.current_price.sqrt(),
+            prev_price.sqrt(),
+            is_ask=False
+        )
+        local_asks.append((prev_price, y))
+        for index, bid_info in enumerate(pool_asks.iloc[::-1].itertuples(index=False)):
+            if index == 0:
+                continue
+            current_tick = Decimal(pool_asks.iloc[index - 1]['tick'].item())
+            current_price = self.tick_to_price(current_tick)
+            y = self._get_token_amount(
+                Decimal(pool_asks.iloc[index - 1]['liq'].item()),
+                current_price.sqrt(),
+                Decimal(self.tick_to_price(pool_asks.iloc[index]['tick'].item())).sqrt(),
+                is_ask=False
+            )
+            local_asks.append((current_price, y))
+        if self.apply_filtering:
+            self.bids.extend([bid for bid in local_asks if price_range[0] < bid[0] < price_range[1]])
+            return
+        self.asks.extend(local_asks)
+
+    def add_bids(self, pool_bids: pd.DataFrame, price_range: tuple[Decimal, Decimal]) -> None:
+        if pool_bids.empty:
+            return
+        local_bids = []
+        prev_tick = Decimal(pool_bids.iloc[0]['tick'].item())
+        prev_price = self.tick_to_price(prev_tick)
+        y = self._get_token_amount(
+            Decimal(pool_bids.iloc[0]['liq'].item()),
+            self.current_price.sqrt(),
+            prev_price.sqrt(),
+            is_ask=False
+        )
+        local_bids.append((prev_price, y))
+        for index, bid_info in enumerate(pool_bids.iloc[::-1].itertuples(index=False)):
+            if index == 0:
+                continue
+            current_tick = Decimal(pool_bids.iloc[index - 1]['tick'].item())
+            current_price = self.tick_to_price(current_tick)
+            y = self._get_token_amount(
+                Decimal(pool_bids.iloc[index]['liq'].item()),
+                current_price.sqrt(),
+                Decimal(self.tick_to_price(pool_bids.iloc[index]['tick'].item())).sqrt(),
+                is_ask=False
+            )
+            local_bids.append((current_price, y))
+        if self.apply_filtering:
+            self.bids.extend([bid for bid in local_bids if price_range[0] < bid[0] < price_range[1]])
+            return
+        self.bids.extend(local_bids)
+
+    def _get_token_amount(
+            self, current_liq: Decimal, current_sqrt: Decimal, next_sqrt: Decimal, is_ask: bool = True
+    ) -> Decimal:
+        """
+        Calculate token amount based on liquidity data and current data processed(asks/bids).
+        :param current_liq: Decimal - Current price liquidity
+        :param current_sqrt: Decimal - Current square root of a price
+        :param next_sqrt: Decimal - Next square root of a price
+        :param is_ask: bool - True if an ask data
+        :return: Decimal - token amount
+        """
+        if is_ask and (current_sqrt == 0 or next_sqrt == 0):
+            raise ValueError("Square root of prices for asks can't be zero.")
+        if not is_ask and next_sqrt == 0:
+            return abs(current_liq / current_sqrt) / self._decimals_diff
+        amount = abs(current_liq / next_sqrt - current_liq / current_sqrt)
+        return amount / self._decimals_diff
 
     def tick_to_price(self, tick: Decimal) -> Decimal:
-        pass
+        return Decimal("1.0001") ** (tick - MAX_MYSWAP_TICK) * Decimal(2 ** 128) * self._decimals_diff
 
     def calculate_liquidity_amount(self, *args, **kwargs) -> Decimal:
         pass
@@ -65,7 +165,10 @@ if __name__ == '__main__':
     print((Decimal(sqrt_price) / Decimal(2**96)) ** Decimal(2) * Decimal(10 ** 12))
 
     # Get price from tick
-    MAX_TICK = 1774532
+    MAX_TICK = Decimal(1774532)
     # Possible formula:
-    price = Decimal("1.0001") ** Decimal(693396 - MAX_TICK) * Decimal(2 ** 128) * Decimal(10 ** 12)
-    print(price)
+    # price = Decimal("1.0001") ** Decimal(693396 - MAX_TICK) * Decimal(2 ** 128) * Decimal(10 ** 12)
+    # tick = (
+    #     math.log(price / (Decimal(2 ** 128) * Decimal(10 ** 12))) / math.log(Decimal("1.0001"))
+    # ) + MAX_MYSWAP_TICK
+    print()
