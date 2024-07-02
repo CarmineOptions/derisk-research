52da690bf7a35078e9f1252e2abdf3a67cd19342
iamnovichek <rungotika@gmail.com>
2024-07-02 10:00:04 +0200

    nostra mainnet added

--- data_handler/handlers/health_ratio_level/health_ratio_handlers.py
+++ data_handler/handlers/health_ratio_level/health_ratio_handlers.py
@@ -11,6 +11,7 @@ from handlers.helpers import TokenValues
 from handlers.state import State, LoanEntity
 from handlers.loan_states.zklend.events import ZkLendState, ZkLendLoanEntity
 from handlers.loan_states.nostra_alpha.events import NostraAlphaState, NostraAlphaLoanEntity
+from handlers.loan_states.nostra_mainnet.events import NostraMainnetState, NostraMainnetLoanEntity
 from handler_tools.constants import ProtocolIDs
 from handlers.liquidable_debt.utils import Prices
 from handlers.liquidable_debt.values import (USER_FIELD_NAME, HEALTH_FACTOR_FIELD_NAME,
@@ -223,6 +224,80 @@ class NostrAlphaHealthRatioHandler(BaseHealthRatioHandler):
                         HEALTH_FACTOR_FIELD_NAME: health_ratio_level,
                         TIMESTAMP_FIELD_NAME: datetime.now().timestamp(),
                         PROTOCOL_FIELD_NAME: ProtocolIDs.NOSTRA_ALPHA.value
+                    }
+                })
+
+        return result_data
+
+
+class NostrMainnetHealthRatioHandler(BaseHealthRatioHandler):
+    """
+    A Nostra Mainnet handler that collects data from DB,
+    computes health_ratio level and stores it in the database.
+
+    :cvar CONNECTOR: A DB connection object.
+    """
+    def __init__(self):
+        super().__init__(state_class=NostraMainnetState, loan_entity_class=NostraMainnetLoanEntity)
+
+    def calculate_health_ratio(self) -> defaultdict:
+        """
+        Calculates health ratio based on provided data.
+        :return: A dictionary of the ready health ratio data.
+        """
+        data, interest_rate_models = self.fetch_data(protocol_name=ProtocolIDs.NOSTRA_MAINNET.value)
+        state = self.state_class()
+
+        for instance in data:
+            loan_entity = self.loan_entity_class()
+
+            loan_entity.debt = TokenValues(values=instance.debt)
+            loan_entity.collateral = TokenValues(values=instance.collateral)
+
+            state.loan_entities.update(
+                {
+                    instance.user: loan_entity,
+                }
+            )
+
+        # Set up collateral and debt interest rate models
+        state.collateral_interest_rate_models = TokenValues(
+            values=interest_rate_models.collateral
+        )
+        state.debt_interest_rate_models = TokenValues(
+            values=interest_rate_models.debt
+        )
+
+        current_prices = Prices()
+        asyncio.run(current_prices.get_lp_token_prices())
+
+        result_data = defaultdict()
+        prices = TokenValues(values=current_prices.prices.values)
+
+        for user_id, loan_entity in state.loan_entities.items():
+            risk_adjusted_collateral_usd = loan_entity.compute_collateral_usd(
+                risk_adjusted=True,
+                collateral_interest_rate_models=state.collateral_interest_rate_models,
+                prices=prices,
+            )
+            risk_adjusted_debt_usd = loan_entity.compute_debt_usd(
+                risk_adjusted=True,
+                debt_interest_rate_models=state.debt_interest_rate_models,
+                prices=prices,
+            )
+            health_ratio_level = loan_entity.compute_health_factor(
+                standardized=False,
+                risk_adjusted_collateral_usd=risk_adjusted_collateral_usd,
+                risk_adjusted_debt_usd=risk_adjusted_debt_usd
+            )
+
+            if self.health_ratio_is_valid(health_ratio_level):
+                result_data.update({
+                    f"{uuid.uuid4()}": {
+                        USER_FIELD_NAME: user_id,
+                        HEALTH_FACTOR_FIELD_NAME: health_ratio_level,
+                        TIMESTAMP_FIELD_NAME: datetime.now().timestamp(),
+                        PROTOCOL_FIELD_NAME: ProtocolIDs.NOSTRA_MAINNET.value
                     }
                 })
 
