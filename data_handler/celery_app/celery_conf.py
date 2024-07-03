e74f86fc71f7314a14e2c4813b088cf2539aec5a
djeck1432 <oleh.kostashchuk@gmail.com>
2024-07-03 14:54:34 +0200

    add new order book task config

--- data_handler/celery_app/celery_conf.py
+++ data_handler/celery_app/celery_conf.py
@@ -20,7 +20,7 @@ app = Celery(
     backend=f"redis://{REDIS_HOST}:{REDIS_PORT}/0",
 )
 
-CRONTAB_TIME = os.environ.get("CRONTAB_TIME", "1")
+CRONTAB_TIME = os.environ.get("CRONTAB_TIME", "5")
 
 app.conf.beat_schedule = {
     # f'run_loan_states_computation_for_hashtack_v0_every_{CRONTAB_TIME}_mins': {
@@ -35,20 +35,24 @@ app.conf.beat_schedule = {
     #     'task': 'run_loan_states_computation_for_zklend',
     #     'schedule': crontab(minute=f'*/{CRONTAB_TIME}'),
     # },
-    f'run_loan_states_computation_for_nostra_alpha_every_{CRONTAB_TIME}_mins': {
-        'task': 'run_loan_states_computation_for_nostra_alpha',
-        'schedule': crontab(minute=f'*/{CRONTAB_TIME}'),
-    },
+    # f'run_loan_states_computation_for_nostra_alpha_every_{CRONTAB_TIME}_mins': {
+    #     'task': 'run_loan_states_computation_for_nostra_alpha',
+    #     'schedule': crontab(minute=f'*/{CRONTAB_TIME}'),
+    # },
     # f'run_loan_states_computation_for_nostra_mainnet_every_{CRONTAB_TIME}_mins': {
     #     'task': 'run_loan_states_computation_for_nostra_mainnet',
     #     'schedule': crontab(minute=f'*/{CRONTAB_TIME}'),
     # },
-    f'run_liquidable_debt_computation_for_zklend_every_{CRONTAB_TIME}_mins': {
-        'task': 'run_liquidable_debt_computation_for_zklend',
-        'schedule': crontab(minute=f'*/{CRONTAB_TIME}'),
-    },
-    "constant_product_market_makers_order_book": {
-        "task": "uniswap_v2_order_book",
+    # f'run_liquidable_debt_computation_for_zklend_every_{CRONTAB_TIME}_mins': {
+    #     'task': 'run_liquidable_debt_computation_for_zklend',
+    #     'schedule': crontab(minute=f'*/{CRONTAB_TIME}'),
+    # },
+    # "constant_product_market_makers_order_book": {
+    #     "task": "uniswap_v2_order_book",
+    #     "schedule": ORDER_BOOK_TIME_INTERVAL,
+    # },
+    "ekubo_order_book": {
+        "task": "ekubo_order_book",
         "schedule": ORDER_BOOK_TIME_INTERVAL,
     },
 }
@@ -63,4 +67,8 @@ app.conf.beat_schedule = {
 #     uniswap_v2_order_book,
 # )
 
+
+from celery_app.order_books_tasks import (
+    ekubo_order_book,
+)
 app.autodiscover_tasks(["celery_app.tasks"])
