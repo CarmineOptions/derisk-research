5dae48308ce09fa19d4fd1c4af01ff937989d297
iamnovichek <rungotika@gmail.com>
2024-07-06 13:31:44 +0200

    run script files per protocol added

---
+++ data_handler/handlers/health_ratio_level/nostra_mainnet.py
@@ -0,0 +1,23 @@
+from db.models import HealthRatioLevel
+
+from handlers.liquidable_debt.values import TIMESTAMP_FIELD_NAME, USER_FIELD_NAME, HEALTH_FACTOR_FIELD_NAME
+
+from health_ratio_handlers import NostrMainnetHealthRatioHandler
+
+
+def run():
+    handler = NostrMainnetHealthRatioHandler()
+
+    data = handler.calculate_health_ratio()
+
+    for id_, health_ratio in data.items():
+        instance = HealthRatioLevel(
+            timestamp=health_ratio[TIMESTAMP_FIELD_NAME],
+            user_id=health_ratio[USER_FIELD_NAME],
+            value=health_ratio[HEALTH_FACTOR_FIELD_NAME]
+        )
+        handler.CONNECTOR.write_to_db(instance)
+
+
+if __name__ == '__main__':
+    run()
