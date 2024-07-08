b961f15180719df073acee407df0dabd3df9f325
lukaspetrasek <petrasek.lks@gmail.com>
2024-07-08 00:58:07 +0000

    update script

--- update_data_continuously.py
+++ update_data_continuously.py
@@ -5,9 +5,6 @@ import update_data
 
 
 if __name__ == "__main__":
-	logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+	logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 	update_data.update_data_continuously()
-	# TODO
-    # zklend_state = src.persistent_state.load_pickle(path=src.persistent_state.PERSISTENT_STATE_FILENAME)
-    # update_data(zklend_state)
