19b5c21c05de211b0a0da655a615848c774a8027
iamnovichek <rungotika@gmail.com>
2024-07-11 14:53:57 +0200

    nostra liquidable debt computing added

--- data_handler/handlers/liquidable_debt/protocols/zklend.py
+++ data_handler/handlers/liquidable_debt/protocols/zklend.py
@@ -1,5 +1,5 @@
 from handlers.liquidable_debt.debt_handlers import (
-    DBLiquidableDebtDataHandler
+    ZkLendDBLiquidableDebtDataHandler
 )
 from handlers.liquidable_debt.values import (
     COLLATERAL_FIELD_NAME, DEBT_FIELD_NAME,
@@ -16,14 +16,14 @@ def run() -> None:
     Runs the liquidable debt computing script for zKlend protocol.
     :return: None
     """
-    handler = DBLiquidableDebtDataHandler(
+    handler = ZkLendDBLiquidableDebtDataHandler(
         loan_state_class=ZkLendState,
         loan_entity_class=ZkLendLoanEntity
     )
 
     data = handler.calculate_liquidable_debt(protocol_name=ProtocolIDs.ZKLEND.value)
 
-    for liquidable_debt_info in data.values():
+    for liquidable_debt_info in data:
         db_row = LiquidableDebt(
             debt_token=liquidable_debt_info[DEBT_FIELD_NAME],
             liquidable_debt=liquidable_debt_info[LIQUIDABLE_DEBT_FIELD_NAME],
@@ -31,7 +31,7 @@ def run() -> None:
             collateral_token_price=liquidable_debt_info[PRICE_FIELD_NAME],
             collateral_token=liquidable_debt_info[COLLATERAL_FIELD_NAME]
         )
-        handler.write_to_db(db_row)
+        handler.db_connector.write_to_db(db_row)
 
 
 if __name__ == '__main__':
