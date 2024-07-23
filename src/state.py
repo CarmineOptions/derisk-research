0dcaff5a4430ae489f425a5ed63ea34b4aa06139
lukaspetrasek <petrasek.lks@gmail.com>
2024-07-23 11:01:20 +0000

    add methods for persisting loan entities

--- src/state.py
+++ src/state.py
@@ -1,8 +1,12 @@
 import abc
 import collections
+import decimal
+import json
+import logging
 
 import pandas
 
+import src.helpers
 import src.types
 
 
@@ -19,12 +23,58 @@ class State(abc.ABC):
         loan_entity_class: src.types.LoanEntity,
         verbose_user: str | None = None,
     ) -> None:
-        self.loan_entity_class: src.type.LoanEntity = loan_entity_class
+        self.loan_entity_class: src.types.LoanEntity = loan_entity_class
         self.verbose_user: str | None = verbose_user
         self.loan_entities: collections.defaultdict = collections.defaultdict(self.loan_entity_class)
         self.interest_rate_models: src.types.CollateralAndDebtInterestRateModels = src.types.CollateralAndDebtInterestRateModels()
         self.token_parameters: src.types.CollateralAndDebtTokenParameters = src.types.CollateralAndDebtTokenParameters()  # TODO: move outside of State?
         self.last_block_number: int = 0
+
+    def set_loan_entities(self, loan_entities: pandas.DataFrame) -> None:
+        # When we're processing the data for the first time, we call this method with empty `loan_entities`. In that
+        # case, there's nothing to be set.
+        if loan_entities.empty:
+            return
+
+        # Clear `self.loan entities``, just in case they were not empty.
+        self.clear_loan_entities()
+
+        # Fill up `self.loan_entities` with `loan_entities`.
+        for _, loan_entity in loan_entities.iterrows():
+            user = loan_entity['user']
+            for collateral_token, collateral_amount in json.loads(loan_entity['collateral'].decode('utf-8')).items():
+                if collateral_amount:
+                    self.loan_entities[user].collateral[collateral_token] = decimal.Decimal(str(collateral_amount))
+            for debt_token, debt_amount in json.loads(loan_entity['debt'].decode('utf-8')).items():
+                if debt_amount:
+                    self.loan_entities[user].debt[debt_token] = decimal.Decimal(str(debt_amount))
+        logging.info(
+            "Set = {} non-zero loan entities out of the former = {} loan entities.".format(
+                len(self.loan_entities),
+                len(loan_entities),
+            )
+        )
+
+    def save_loan_entities(self, path: str) -> None:
+        loan_entities = []
+        for user, loan_entity in self.loan_entities.items():
+            # Convert each loan entity into a feasible format.
+            loan_entities.append(
+                {
+                    'user': user,
+                    'collateral': {x: int(y) for x, y in loan_entity.collateral.items()},
+                    'debt': {x: int(y) for x, y in loan_entity.debt.items()},
+                }
+
+            )
+        loan_entities = pandas.DataFrame(loan_entities)
+        src.helpers.save_dataframe(data = loan_entities, path = path)
+        logging.info("Saved = {} loan entities.".format(len(loan_entities)))
+
+    def clear_loan_entities(self) -> None:
+        logging.info("Clearing = {} loan entities.".format(len(self.loan_entities)))
+        self.loan_entities = collections.defaultdict(self.loan_entity_class)
+
 
     # TODO: This method will likely differ across protocols. -> Leave undefined?
     def process_event(self, event: pandas.Series) -> None:
