f978e4a029b5e172c7d19caf827058bca71290a0
djeck1432 <oleh.kostashchuk@gmail.com>
2024-08-07 13:51:42 +0200

    add new logic for has skip

--- data_handler/handlers/loan_states/hashtack_v0/utils.py
+++ data_handler/handlers/loan_states/hashtack_v0/utils.py
@@ -1,6 +1,7 @@
 from db.crud import InitializerDBConnector
 from db.models import HashtackCollateralDebt
 from decimal import Decimal
+from handlers.loan_states.hashtack_v0.events import HashstackV0LoanEntity
 
 
 class HashtackV0Initializer:
@@ -15,7 +16,7 @@ class HashtackV0Initializer:
 
         :param users_ids: The list of user ids to set the loan states for.
         """
-        loan_states = self.db_connector.get_by_user_ids(users_ids)
+        loan_states = self.db_connector.get_hashtackv0_by_user_ids(users_ids)
         for loan_state in loan_states:
             self._set_loan_state_per_user(loan_state)
 
@@ -25,7 +26,11 @@ class HashtackV0Initializer:
 
         :param loan_state: The loan state data.
         """
-        user_loan_state = self.hashtack_state.loan_entities[loan_state.user_id]
+        self.hashtack_state.loan_entities[loan_state.loan_id] = HashstackV0LoanEntity(
+            user=loan_state.user_id,
+            debt_category=loan_state.debt_category
+        )
+        user_loan_state = self.hashtack_state.loan_entities[loan_state.loan_id]
         user_loan_state.debt_category = loan_state.debt_category
         user_loan_state.original_collateral.values = self._convert_float_to_decimal(
             loan_state.original_collateral
@@ -37,6 +42,9 @@ class HashtackV0Initializer:
             loan_state.collateral
         )
         user_loan_state.debt.values = self._convert_float_to_decimal(loan_state.debt)
+
+        # extra field to avoid saving to db
+        user_loan_state.has_skip = True
 
     @staticmethod
     def _convert_float_to_decimal(data: dict | None) -> dict | None:
