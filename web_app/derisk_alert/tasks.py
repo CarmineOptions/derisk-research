8d95e3c14cd75f73b4b461694df5235478a2eeef
djeck1432 <oleh.kostashchuk@gmail.com>
2024-08-16 11:21:59 +0200

    add async code

--- web_app/derisk_alert/tasks.py
+++ web_app/derisk_alert/tasks.py
@@ -20,6 +20,29 @@ connector = DBConnector()
 notificator = TelegramNotifications(db_connector=connector)
 
 
+# @app.task(name="check_health_ratio_level_changes")
+# def check_health_ratio_level_changes():
+#     logger.info("Checking health ratio level changes")
+#     # update_data()
+#     logger.info("Data updated")
+#
+#     subscribers = get_all_activated_subscribers_from_db()
+#     logger.info(f"Found {len(subscribers)} subscribers")
+#     for subscriber in subscribers:
+#         health_ratio_level = compute_health_ratio_level(
+#             protocol_name=subscriber.protocol_id.value, user_id=subscriber.wallet_id
+#         )
+#
+#         # if (
+#         #         calculate_difference(health_ratio_level, subscriber.health_ratio_level)
+#         #         <= HEALTH_RATIO_LEVEL_ALERT_VALUE
+#         # ):
+#         print(f"Subscriber {subscriber.id} has health ratio level {health_ratio_level}")
+#         logger.info(f"Subscriber {subscriber.id} has health ratio level {health_ratio_level}")
+#         notificator.send_notification(notification_id=subscriber.id)
+#     logger.info("Health ratio level changes checked")
+#     asyncio.run(notificator(is_infinity=True))
+
 @app.task(name="check_health_ratio_level_changes")
 def check_health_ratio_level_changes():
     logger.info("Checking health ratio level changes")
@@ -28,18 +51,20 @@ def check_health_ratio_level_changes():
 
     subscribers = get_all_activated_subscribers_from_db()
     logger.info(f"Found {len(subscribers)} subscribers")
-    for subscriber in subscribers:
+
+    async def process_subscriber(subscriber):
         health_ratio_level = compute_health_ratio_level(
             protocol_name=subscriber.protocol_id.value, user_id=subscriber.wallet_id
         )
 
-        # if (
-        #         calculate_difference(health_ratio_level, subscriber.health_ratio_level)
-        #         <= HEALTH_RATIO_LEVEL_ALERT_VALUE
-        # ):
-        print(f"Subscriber {subscriber.id} has health ratio level {health_ratio_level}")
         logger.info(f"Subscriber {subscriber.id} has health ratio level {health_ratio_level}")
-        notificator.send_notification(notification_id=subscriber.id)
+
+        await notificator.send_notification(notification_id=subscriber.id)
+
+    loop = asyncio.get_event_loop()
+    tasks = [process_subscriber(subscriber) for subscriber in subscribers]
+    loop.run_until_complete(asyncio.gather(*tasks))
+
     logger.info("Health ratio level changes checked")
     asyncio.run(notificator(is_infinity=True))
 
