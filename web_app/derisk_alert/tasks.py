26c20314d10251807c7c916dee961dc251a55577
djeck1432 <oleh.kostashchuk@gmail.com>
2024-08-16 11:15:15 +0200

    fix logical issue

--- web_app/derisk_alert/tasks.py
+++ web_app/derisk_alert/tasks.py
@@ -30,7 +30,7 @@ def check_health_ratio_level_changes():
     logger.info(f"Found {len(subscribers)} subscribers")
     for subscriber in subscribers:
         health_ratio_level = compute_health_ratio_level(
-            protocol_name=subscriber.protocol_id.value, user_id=subscriber.id
+            protocol_name=subscriber.protocol_id.value, user_id=subscriber.wallet_id
         )
 
         # if (
@@ -42,3 +42,30 @@ def check_health_ratio_level_changes():
         notificator.send_notification(notification_id=subscriber.id)
     logger.info("Health ratio level changes checked")
     asyncio.run(notificator(is_infinity=True))
+
+
+# def check_health_ratio_level_changes():
+#     logger.info("Checking health ratio level changes")
+#     # update_data()
+#     logger.info("Data updated")
+#
+#     subscribers = get_all_activated_subscribers_from_db()
+#     logger.info(f"Found {len(subscribers)} subscribers")
+#     for subscriber in subscribers:
+#         health_ratio_level = compute_health_ratio_level(
+#             protocol_name=subscriber.protocol_id.value, user_id=subscriber.wallet_id
+#         )
+#
+#         # if (
+#         #         calculate_difference(health_ratio_level, subscriber.health_ratio_level)
+#         #         <= HEALTH_RATIO_LEVEL_ALERT_VALUE
+#         # ):
+#         print(f"Subscriber {subscriber.id} has health ratio level {health_ratio_level}")
+#         logger.info(f"Subscriber {subscriber.id} has health ratio level {health_ratio_level}")
+#         notificator.send_notification(notification_id=subscriber.id)
+#     logger.info("Health ratio level changes checked")
+#     asyncio.run(notificator(is_infinity=True))
+#
+#
+# if __name__ == "__main__":
+#     check_health_ratio_level_changes()
