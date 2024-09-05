9ea38b9db4f78607580ff38719d4689f2e686fa0
djeck1432 <oleh.kostashchuk@gmail.com>
2024-09-05 16:59:13 +0200

    add int chat id

--- data_handler/error_handler/config.py
+++ data_handler/error_handler/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
-ERROR_CHAT_ID = int(os.environ.get("ERROR_CHAT_ID", None))
+ERROR_CHAT_ID = int(os.environ.get("ERROR_CHAT_ID", 123))
 
 REDIS_HOST = os.environ.get("REDIS_HOST", "")
 REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
