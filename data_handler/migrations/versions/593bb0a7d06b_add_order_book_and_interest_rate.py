baf920c469e054b0585e38077786eaa9711d5fb0
djeck1432 <oleh.kostashchuk@gmail.com>
2024-10-23 19:52:59 +0200

    refactoring

--- data_handler/migrations/versions/593bb0a7d06b_add_order_book_and_interest_rate.py
+++ data_handler/migrations/versions/593bb0a7d06b_add_order_book_and_interest_rate.py
@@ -11,7 +11,8 @@ from typing import Sequence, Union
 import sqlalchemy as sa
 import sqlalchemy_utils
 from alembic import op
-from handler_tools.constants import ProtocolIDs
+
+from shared.constants import ProtocolIDs
 
 # revision identifiers, used by Alembic.
 revision: str = "593bb0a7d06b"
