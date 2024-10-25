c57a18b006e91d2473c0aec2316c19a7c6aa4d2e
Gerson2102 <gersonloavas@gmail.com>
2024-10-25 07:03:18 -0600

    Addressing PR comments, adding docs

--- data_handler/handler_tools/data_parser/serializers.py
+++ data_handler/handler_tools/data_parser/serializers.py
@@ -4,10 +4,31 @@ from pydantic import BaseModel, field_validator
 
 
 class DataAccumulatorsSyncEvent(BaseModel):
+    """
+    Model to parse and validate data for AccumulatorsSync event.
+
+    This model validates and converts the lending and debt accumulators from hexadecimal
+    strings to `Decimal` format, scaled by `1e27`.
+
+    Attributes:
+        token (str): The token address as a hexadecimal string.
+        lending_accumulator (Decimal): The lending accumulator value, converted from hex to Decimal.
+        debt_accumulator (Decimal): The debt accumulator value, converted from hex to Decimal.
+    """
+
     token: str
     lending_accumulator: Decimal
     debt_accumulator: Decimal
 
     @field_validator("lending_accumulator", "debt_accumulator", mode="before")
-    def hex_to_decimal(cls, v):
+    def hex_to_decimal(cls, v: str) -> Decimal:
+        """
+        Converts a hexadecimal string to a Decimal value, scaled by 1e27.
+
+        Args:
+            v (str): The hexadecimal string to be converted.
+
+        Returns:
+            Decimal: The converted decimal value scaled by 1e27.
+        """
         return Decimal(int(v, 16)) / Decimal("1e27")
