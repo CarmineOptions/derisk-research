a9e837a63268bb44bb403f016e69107d83a18148
djeck1432 <oleh.kostashchuk@gmail.com>
2024-10-28 16:46:30 +0100

    update celery set up

--- apps/data_handler/celery_app/celery_conf.py
+++ apps/data_handler/celery_app/celery_conf.py
@@ -58,8 +58,8 @@ app.conf.beat_schedule = {
 }
 
 from data_handler.celery_app.order_books_tasks import ekubo_order_book
-from data_handler.celery_app.tasks import (  # run_loan_states_computation_for_hashtack_v0,; run_loan_states_computation_for_hashtack_v1,; run_loan_states_computation_for_nostra_alpha,; run_loan_states_computation_for_nostra_mainnet,; run_loan_states_computation_for_zklend,; run_liquidable_debt_computation_for_nostra_alpha,; run_liquidable_debt_computation_for_nostra_mainnet,; run_liquidable_debt_computation_for_hashstack_v0,; run_liquidable_debt_computation_for_hashstack_v1,; uniswap_v2_order_book,
+from data_handler.celery_app.tasks import (
     run_liquidable_debt_computation_for_zklend,
-)
+)  # run_loan_states_computation_for_hashtack_v0,; run_loan_states_computation_for_hashtack_v1,; run_loan_states_computation_for_nostra_alpha,; run_loan_states_computation_for_nostra_mainnet,; run_loan_states_computation_for_zklend,; run_liquidable_debt_computation_for_nostra_alpha,; run_liquidable_debt_computation_for_nostra_mainnet,; run_liquidable_debt_computation_for_hashstack_v0,; run_liquidable_debt_computation_for_hashstack_v1,; uniswap_v2_order_book,
 
 app.autodiscover_tasks(["celery_app.tasks", "celery_app.order_books_tasks"])
