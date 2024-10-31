877fb677780aae513cebf9979ed999962493ebc9
sigmadawg <nik.insync@gmail.com>
2024-11-01 03:39:00 +0530

    .

--- apps/data_handler/handlers/loan_states/zklend/__init__.py
+++ apps/data_handler/handlers/loan_states/zklend/__init__.py
@@ -1,8 +1,1 @@
 """ This module contains the settings for the ZkLend loan states handler. """
-from data_handler.handlers.loan_states.zklend.settings import (
-    (
-    TOKEN_SETTINGS,
-   
-    TokenSettings,
-),
-)
