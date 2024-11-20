98c3a14a9fd80dafc9bc514a292c1a431f15d3c5
djeck1432 <oleh.kostashchuk@gmail.com>
2024-11-20 20:45:21 +0100

    add new file

---
+++ apps/data_handler/handler_tools/data_parser/nostra.py
@@ -0,0 +1,30 @@
+
+
+
+class NostraDataParser:
+    """
+    Parses the nostra data to human-readable format.
+    """
+    def parse_interest_rate_model_event(self):
+        pass
+
+    def parse_non_interest_bearing_collateral_mint_event(self):
+        pass
+
+    def parse_non_interest_bearing_collateral_burn_event(self):
+        pass
+
+    def parse_interest_bearing_collateral_mint_event(self):
+        pass
+
+    def parse_interest_bearing_collateral_burn_event(self):
+        pass
+
+    def parse_debt_transfer_event(self):
+        pass
+
+    def parse_debt_mint_event(self):
+        pass
+
+    def parse_debt_burn_event(self):
+        pass
