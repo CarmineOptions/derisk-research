e053732426ea2a812827d779ba9653c5f164020c
Ekuma Matthew <79433184+ekumamatthew@users.noreply.github.com>
2024-11-23 17:29:53 +0000

    feat/fix issue-#288 remove spacing

--- apps/data_handler/tests/conftest.py
+++ apps/data_handler/tests/conftest.py
@@ -3,7 +3,6 @@ This module contains the fixtures for the tests.
 """
 
 from unittest.mock import MagicMock
-
 import pytest
 from data_handler.db.crud import (
     DBConnector,
@@ -12,55 +11,26 @@ from data_handler.db.crud import (
 )
 from data_handler.handler_tools.api_connector import DeRiskAPIConnector
 from data_handler.handler_tools.data_parser.zklend import ZklendDataParser
-
-
 @pytest.fixture(scope="module")
-def mock_db_connector() -> None:
-    """
-    Mock DBConnector
-    :return: None.
-    """
+def mock_db_connector() -> None:"""Mock DBConnector:return: None."""
     mock_connector = MagicMock(spec=DBConnector)
     mock_connector.get_last_block.return_value = 12345
     yield mock_connector
-
-
 @pytest.fixture(scope="module")
-def mock_initializer_db_connector() -> None:
-    """
-    Mock for InitializerDBConnector
-    :return: None
-    """
+def mock_initializer_db_connector() -> None:"""Mock for InitializerDBConnector:return: None"""
     mock_initializer_connector = MagicMock(spec=InitializerDBConnector)
       mock_initializer_connector.engine = MagicMock()
     mock_initializer_connector.connection = MagicMock()
     yield mock_initializer_connector
-
-
 @pytest.fixture(scope="function")
-def mock_api_connector():
-    """
-    Creates a mock API connector for testing.
-    """
+def mock_api_connector(): """Creates a mock API connector for testing."""
     mock_api_connector = MagicMock(spec=DeRiskAPIConnector)
     yield mock_api_connector
-
-
 @pytest.fixture(scope="function")
-def mock_zklend_event_db_connector():
-    """
-    Mock for ZkLendEventDBConnector
-    :return: None
-    """
+def mock_zklend_event_db_connector(): """Mock for ZkLendEventDBConnector:return: None"""
     mock_zklend_event_db_connector = MagicMock(spec=ZkLendEventDBConnector)
     yield mock_zklend_event_db_connector
-
-
 @pytest.fixture(scope="function")
-def mock_zklend_data_parser():
-    """
-    Mock for ZklendDataParser
-    :return: None
-    """
+def mock_zklend_data_parser():""" Mock for ZklendDataParser :return: None """
     mock_zklend_data_parser = MagicMock(spec=ZklendDataParser)
     yield mock_zklend_data_parser
