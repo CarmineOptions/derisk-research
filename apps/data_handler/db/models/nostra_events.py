90dc976c29282f0ed06b81997fefdbd049ef3f41
MooreTheAnalyst <162017070+MooreTheAnalyst@users.noreply.github.com>
2024-11-26 11:41:59 +0000

    feat: Interest Rate and Bearing models

--- apps/data_handler/db/models/nostra_events.py
+++ apps/data_handler/db/models/nostra_events.py
@@ -55,3 +55,33 @@ class DebtTransferEventModel(EventBaseModel):
     sender: Mapped[str] = mapped_column(String, nullable=False)
     recipient: Mapped[str] = mapped_column(String, nullable=False)
     amount: Mapped[Decimal] = mapped_column(Numeric(38, 18), nullable=False)
+
+
+class InterestRateModelEventModel(EventBaseModel):
+    """
+    Database model for InterestRate event, inheriting from EventBaseModel.
+
+    This model stores the
+        debt_token (str): The address of the debt token.
+        lending_index (Decimal): The lending index in hexadecimal.
+        borrow_index (Decimal): The borrow index in hexadecimal.
+    """
+
+    __tablename__ = "interest_rate_event"
+
+    debt_token: Mapped[str] = mapped_column(String, nullable=False)
+    lending_index: Mapped[Decimal] = mapped_column(Numeric(38, 18), nullable=False)
+    borrow_index: Mapped[Decimal] = mapped_column(Numeric(38, 18), nullable=False)
+
+
+class BearingCollateralMintEventModel(EventBaseModel):
+    """
+    Database model for BearingCollateral mint event, inheriting from EventBaseModel.
+
+    This model stores the user address and the amount minted.
+    """
+
+    __tablename__ = "bearing_collateral_mint_event"
+
+    user: Mapped[str] = mapped_column(String, nullable=False)
+    amount: Mapped[Decimal] = mapped_column(Numeric(38, 18), nullable=False)
