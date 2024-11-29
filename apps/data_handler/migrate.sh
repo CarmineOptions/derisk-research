754a4be5e4f589408c672f39475b3704a4f65b02
vitaliypopel <vitaliypopel14@gmail.com>
2024-11-29 10:37:17 +0200

    Added missed commands to migrate script

--- apps/data_handler/migrate.sh
+++ apps/data_handler/migrate.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+echo "Running DB in Docker container..."
+docker-compose -f ../devops/dev/docker-compose.db.yaml up -d --remove-orphans
+
+echo "Installing all dependencies for \"data_handler\" with Poetry..."
+cd data_handler
+poetry install
+
+echo "Activating environment..."
+poetry shell
+cd ..
+
 echo "Applying latest existing migrations..."
 alembic -c data_handler/alembic.ini upgrade head
 
@@ -7,6 +18,3 @@ read -p "Enter migration message: " migration
 
 echo "Generating new \"$migration\" migration..."
 alembic -c data_handler/alembic.ini revision --autogenerate -m "$migration"
-
-echo "Applying new migration..."
-alembic -c data_handler/alembic.ini upgrade head
