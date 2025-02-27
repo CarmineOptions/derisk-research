610385b9913c1c12393c3a5886f3ba1296c58339
djeck1432 <oleh.kostashchuk@gmail.com>
2025-02-27 18:25:19 +0100

    fix Dockerfile issues

--- apps/dashboard_app/.entrypoint.sh
+++ apps/dashboard_app/.entrypoint.sh
@@ -3,13 +3,6 @@
 set -e  # Exit immediately if any command fails
 
 # Wait for the database to be ready
-echo "Waiting for database to be ready..."
-until PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USER" -d "$DB_NAME" -h "$DB_HOST" -p "$DB_PORT" -c '\q'; do
-  echo "$POSTGRES_USER" "$POSTGRES_DB"
-  sleep 2
-done
-echo "Database is ready."
-
 # Import data from dump file
 if [ -f ./derisk_dump_part_aa.sql ]; then
   echo "Importing data from derisk_dump_part_aa.sql..."
@@ -35,6 +28,6 @@ else
   done
 fi
 
-# Start Streamlit
-echo "Starting Streamlit application..."
-streamlit run dashboard.py --server.port=8501 --server.address=0.0.0.0
+## Start Streamlit
+#echo "Starting Streamlit application..."
+#streamlit run dashboard.py --server.port=8501 --server.address=0.0.0.0
