c3da98d1d4e67abbe9581e1ba908eed73edada46
QueensleyC <ebyqueensley1@gmail.com>
2025-03-23 19:35:38 +0100

    Move models from watcher to dashboard_app

---
+++ apps/dashboard_app/app/models/base.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+    """
+    Base class for ORM models.
+
+    :ivar id: The unique identifier of the entity.
+    """
+
+    id: Mapped[UUID] = Column(UUID, default=uuid4, primary_key=True)
+    metadata = MetaData()
