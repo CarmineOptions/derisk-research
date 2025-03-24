2691e91607f01b7471e4895a905e92225ddc68f4
Dan0m1 <bohdan.shevchenko182@gmail.com>
2025-03-24 20:31:53 +0200

    Fix import issues

--- apps/dashboard_app/tests/test_watcher_mixin.py
+++ apps/dashboard_app/tests/test_watcher_mixin.py
@@ -8,9 +8,9 @@ import pytest
 from sqlalchemy import Float, String
 from sqlalchemy.orm import Mapped, Session, mapped_column
 
-from ..models.base import Base
-from ..utils.values import CreateSubscriptionValues
-from ..utils.watcher_mixin import WatcherMixin
+from dashboard_app.app.models.base import Base
+from dashboard_app.app.utils.values import CreateSubscriptionValues
+from dashboard_app.app.utils.watcher_mixin import WatcherMixin
 
 
 class MockModel(Base):
