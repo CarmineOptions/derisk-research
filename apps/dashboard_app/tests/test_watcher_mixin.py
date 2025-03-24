2a79a46a59aadf1425016cbf99f37def8c364e29
Dan0m1 <bohdan.shevchenko182@gmail.com>
2025-03-24 15:42:43 +0200

    Add docstring and formatting

--- apps/dashboard_app/tests/test_watcher_mixin.py
+++ apps/dashboard_app/tests/test_watcher_mixin.py
@@ -1,4 +1,3 @@
-from typing import Type
 from unittest.mock import MagicMock, patch
 
 import pytest
@@ -21,11 +20,6 @@ class MockModel(Base):
         return f"<Mock(test_attr={self.test_attr}, health_ratio={self.health_ratio_level})>"
 
 
-@pytest.fixture
-def mock_db_session():
-    return MagicMock()
-
-
 @pytest.mark.parametrize(
     "health_ratio, expected",
     [
@@ -38,7 +32,11 @@ def mock_db_session():
     ],
 )
 def test_health_ratio_is_valid(health_ratio, expected):
+    """
+    Test the health_ratio_is_valid method against different scenarios.
+    """
     result = WatcherMixin.health_ratio_is_valid(health_ratio)
+
     assert result == expected
 
 
@@ -59,22 +57,36 @@ invalid_health_ratio_response = {
     ],
 )
 def test_validate_health_ratio(health_ratio, expected):
+    """
+    Test the validate_health_ratio method against different scenarios.
+    """
     result = WatcherMixin.validate_health_ratio(health_ratio)
+
     assert result == expected
 
 
 @pytest.mark.parametrize(
-    "exists_in_db_value, fields_value, health_ratio_level, expected_dict",
+    "exists_in_db, unique_fields, health_ratio_level, expected_validation_errors",
     [
+        # Test when there are no unique fields.
+        # health_ratio_level is valid (0 <= 10.0 <= 10.0)
         (True, (), 5.0, {}),
+        # Test when there is one unique field which test object contain,and it already exists in DB.
+        # health_ratio_level is valid (0 <= 5.0 <= 10.0)
         (True, ("test_attr"), 5.0, {"test_attr": "Current test attr is already taken"}),
+        # Test when there is one unique field which test object contain, but it doesn't exist in DB yet.
+        # health_ratio_level is valid (0 <= 10.0 <= 10.0)
         (False, ("test_attr"), 10.0, {}),
+        # Test when there is one unique field which test object contain, but it doesn't exist in DB yet.
+        # health_ratio_level is invalid (10.0 <= 11.0)
         (
             False,
             ("test_attr"),
             11.0,
             {"health_ratio_level": "Your health ratio level must be between 0 and 10"},
         ),
+        # Test when there is one unique field which test object contain, and it already exists in DB.
+        # health_ratio_level is invalid (10.0 <= 11.0)
         (
             True,
             ("test_attr"),
@@ -87,21 +99,25 @@ def test_validate_health_ratio(health_ratio, expected):
     ],
 )
 def test_validate_fields(
-    exists_in_db_value: bool,
-    fields_value: tuple,
+    exists_in_db: bool,
+    unique_fields: tuple,
     health_ratio_level: float,
-    expected_dict: dict,
-    mock_db_session,
+    expected_validation_errors: dict,
 ):
+    """
+    Test the validate_fields method against different scenarios.
+    """
     with patch(
         "app.utils.watcher_mixin.WatcherMixin._exists_in_db",
-        return_value=exists_in_db_value,
+        return_value=exists_in_db,
+    ), patch(
+        "app.utils.values.NotificationValidationValues.unique_fields", unique_fields
     ):
         mock_obj = MockModel(test_attr="test", health_ratio_level=health_ratio_level)
-        with patch(
-            "app.utils.values.NotificationValidationValues.unique_fields", fields_value
-        ):
-            result = WatcherMixin.validate_fields(
-                db=mock_db_session, obj=mock_obj, model=MockModel
-            )
-            assert result == expected_dict
+        mock_db = MagicMock(spec=Session)
+
+        validation_result = WatcherMixin.validate_fields(
+            db=mock_db, obj=mock_obj, model=MockModel
+        )
+
+        assert validation_result == expected_validation_errors
