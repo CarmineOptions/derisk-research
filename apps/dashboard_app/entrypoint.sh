d45e21fc6e54cd5b78d40ca5e9ba3407713c11c8
djeck1432 <oleh.kostashchuk@gmail.com>
2025-03-25 15:14:52 +0100

    fix issue with db connection

--- apps/dashboard_app/entrypoint.sh
+++ apps/dashboard_app/entrypoint.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+echo "POSTGRES_USER: $POSTGRES_USER"
+echo "POSTGRES_PASSWORD: $POSTGRES_PASSWORD"
+echo "POSTGRES_DB: $POSTGRES_DB"
+echo "DB_HOST: $DB_HOST"
+echo "DB_PORT: $DB_PORT"
+
+#!/bin/bash
 set -e  # Exit immediately if any command fails
 
 # Import data from dump file
 if [ -f ./derisk_dump_part_aa.sql ]; then
   echo "Importing data from derisk_dump_part_aa.sql..."
-  PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USER" -d "$DB_NAME" -h "$DB_HOST" -p "$DB_PORT" -f ./derisk_dump_part_aa.sql
+  PGPASSWORD="$POSTGRES_PASSWORD" psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" \
+    -h "$DB_HOST" -p "$DB_PORT" -f ./derisk_dump_part_aa.sql
   echo "Data import complete."
 else
   echo "No ./derisk_dump_part_aa.sql file found."
