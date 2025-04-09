e20bbabb0ad054231db555f038b847f93cb6d96c
zimaexe <zima2908@gmail.com>
2025-04-09 19:49:34 +0200

    added new files in config for api key

--- apps/dashboard_app/app/core/config.py
+++ apps/dashboard_app/app/core/config.py
@@ -1,4 +1,8 @@
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
+class Settings(BaseSettings):
     # Database settings
     db_driver: str = "postgresql+asyncpg"
     db_name: str = Field(default="db_name", alias="POSTGRES_DB")
@@ -7,6 +11,10 @@
     db_host: str = Field(default="localhost", alias="DB_HOST")
     db_port: int = 5432
 
-    derisk_api_url: str = Field(..., env="DERISK_API_URL") 
-    network: str = Field(default="sepolia", alias="NETWORK")  
+    derisk_api_url: str = Field(..., env="DERISK_API_URL")
+    network: str = Field(default="sepolia", alias="NETWORK")
     active_protocols: list = Field(default_factory=lambda: ["ZkLend", "NostraMainnet", "NostraAlpha"], alias="ACTIVE_PROTOCOLS")
+
+    coingecko_api_key: str = "api_key"
+
+settings = Settings()
