8a0f148e4df530ad55636d7ecb4fb56410df41b7
zimaexe <zima2908@gmail.com>
2025-04-09 19:51:15 +0200

    addeddocksstrings

--- apps/dashboard_app/app/services/token_price.py
+++ apps/dashboard_app/app/services/token_price.py
@@ -8,6 +8,16 @@ headers = {
 }
 
 async def get_all_prices(date: str):
+    """
+    Fetches the historical prices of all tokens for a given date.
+    This asynchronous function iterates through the tokens defined in `TOKEN_SETTINGS`
+    and retrieves their prices for the specified date using the `get_token_price` function.
+    Args:
+        date (str): The date for which to fetch token prices, formatted as 'YYYY-MM-DD'.
+    Returns:
+        dict: A dictionary where the keys are token symbols and the values are their respective prices.
+    """
+
     prices = dict()
     for token in TOKEN_SETTINGS:
         prices[f'{token.symbol}'] = await get_token_price(coin_id=token.coind_id, date=date)
@@ -16,6 +26,17 @@ async def get_all_prices(date: str):
 
 
 async def get_token_price(coin_id: str, date: str):
+    """
+    Fetches the historical price of a cryptocurrency in USD for a specific date.
+    Args:
+        coin_id (str): The unique identifier of the cryptocurrency (e.g., 'bitcoin', 'ethereum').
+        date (str): The date for which the price is requested, in the format 'dd-mm-yyyy'.
+    Returns:
+        float: The price of the cryptocurrency in USD on the specified date.
+    Raises:
+        Exception: If the API request fails or the response does not contain the expected data.
+    """
+
     url = f"https://api.coingecko.com/api/v3/coins/{coin_id}/history?date={date}"
     return (await api_request(url=url, headers = headers, key='market_data')).get("current_price").get("usd")
 
